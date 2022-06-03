5ea841681d3d7f5bd99c05ee5be7fff040ef82f2
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2022-06-03 16:13:41 +0200

    [PASSBE-131] Create Unit Tests & UI Tests targets for iOS project

---
+++ iOS/iOSUITests/iOSUITestsLaunchTests.swift
@@ -0,0 +1,45 @@
+//
+// iOSUITestsLaunchTests.swift
+// Proton Key - Created on 03/06/2022.
+// Copyright (c) 2022 Proton Technologies AG
+//
+// This file is part of Proton Key.
+//
+// Proton Key is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Key is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Key. If not, see https://www.gnu.org/licenses/.
+
+import XCTest
+
+class iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
