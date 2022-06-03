22482b492a0ce690b0498319483c8d43f306e43e
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2022-06-03 16:37:05 +0200

    [PASSBE-131] Create Unit tests & UI tests target for macOS project

--- macOS/macOSUITests/macOSUITests.swift
+++ macOS/macOSUITests/macOSUITests.swift
@@ -1,5 +1,5 @@
 //
-// iOSUITestsLaunchTests.swift
+// macOSUITests.swift
 // Proton Key - Created on 03/06/2022.
 // Copyright (c) 2022 Proton Technologies AG
 //
@@ -20,26 +20,26 @@
 
 import XCTest
 
-class iOSUITestsLaunchTests: XCTestCase {
-
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
-        true
-    }
+class macOSUITests: XCTestCase {
 
     override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    func testLaunch() throws {
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
 
-        // Insert steps here to perform after app launch but before taking a screenshot,
-        // such as logging into a test account or navigating somewhere in the app
-
-        let attachment = XCTAttachment(screenshot: app.screenshot())
-        attachment.name = "Launch Screen"
-        attachment.lifetime = .keepAlways
-        add(attachment)
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 }
