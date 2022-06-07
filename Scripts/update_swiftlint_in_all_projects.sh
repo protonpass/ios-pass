9801e3a82eb40c0792d87b369a478819f8187403
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2022-06-07 18:30:50 +0200

    [PASSBE-137] Fix script wording

--- Scripts/update_swiftlint_in_all_projects.sh
+++ Scripts/update_swiftlint_in_all_projects.sh
@@ -6,7 +6,7 @@ script_dir=$(basename ${script_path})
 script_name=$(basename "$0")
 base_file_name="base_swiftlint.yml"
 base_content=$(cat "${script_path}/${base_file_name}")
-final_content="# This file is auto-generated and should not be modified.\n# Modify instead ${script_dir}/${base_file_name}.\n# Then run ${script_dir}/${script_name}.\n${base_content}"
+final_content="# This file is auto-generated and should not be modified manually.\n# Modify instead ${script_dir}/${base_file_name}.\n# Then run ${script_dir}/${script_name}.\n${base_content}"
 modules=("Client" "Core" "iOS" "macOS" "UIComponents")
 
 for module in "${modules[@]}"
@@ -15,4 +15,4 @@ do
 done
 
 # echo with colors: https://stackoverflow.com/a/57559997/2034535
-echo "\033[32;1m🐶 Updated .swiftlint.yml files in all projects\033[0m"
+echo "\033[32;1mUpdated .swiftlint.yml files in all projects\033[0m"
