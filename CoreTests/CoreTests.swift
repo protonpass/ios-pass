db42e91a637b7f93059de0c88056b6d44aa16ead
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2022-07-01 23:08:57 +0200

    Fix linting errors

--- CoreTests/CoreTests.swift
+++ CoreTests/CoreTests.swift
@@ -18,8 +18,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Proton Key. If not, see https://www.gnu.org/licenses/.
 
-import XCTest
 @testable import Core
+import XCTest
 
 class CoreTests: XCTestCase {
     func testCoreDummyStruct() {
--- UIComponents/DetailView.swift
+++ UIComponents/DetailView.swift
@@ -22,6 +22,7 @@ import SwiftUI
 
 public struct DetailView: View {
     public init() {}
+
     public var body: some View {
         Text("Hello from UIComponents")
     }
