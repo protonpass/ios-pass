cf8c178ef14c1b3950ddfb53ff38005a9359f617
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2022-07-05 15:59:36 +0200

    [PASSBE-153] "Proton Key" -> "Proton Pass"

--- CoreTests/CoreTests.swift
+++ CoreTests/CoreTests.swift
@@ -1,22 +1,22 @@
 //
 // CoreTests.swift
-// Proton Key - Created on 01/07/2022.
+// Proton Pass - Created on 01/07/2022.
 // Copyright (c) 2022 Proton Technologies AG
 //
-// This file is part of Proton Key.
+// This file is part of Proton Pass.
 //
-// Proton Key is free software: you can redistribute it and/or modify
+// Proton Pass is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Proton Key is distributed in the hope that it will be useful,
+// Proton Pass is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Proton Key. If not, see https://www.gnu.org/licenses/.
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
 
 @testable import Core
 import XCTest
--- iOS/Scenes/Welcome/WelcomeCoordinator.swift
+++ iOS/Scenes/Welcome/WelcomeCoordinator.swift
@@ -1,22 +1,22 @@
 //
 // WelcomeCoordinator.swift
-// Proton Key - Created on 02/07/2022.
+// Proton Pass - Created on 02/07/2022.
 // Copyright (c) 2022 Proton Technologies AG
 //
-// This file is part of Proton Key.
+// This file is part of Proton Pass.
 //
-// Proton Key is free software: you can redistribute it and/or modify
+// Proton Pass is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Proton Key is distributed in the hope that it will be useful,
+// Proton Pass is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Proton Key. If not, see https://www.gnu.org/licenses/.
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
 
 import Client
 import Core
@@ -54,10 +54,10 @@ final class WelcomeCoordinator: Coordinator {
     }()
 
     private lazy var logInAndSignUp: LoginAndSignup = {
-        let summaryScreenVariant = SummaryScreenVariant.screenVariant(.drive("Start using Proton Key"))
+        let summaryScreenVariant = SummaryScreenVariant.screenVariant(.drive("Start using Proton Pass"))
         let signUpParameters = SignupParameters(passwordRestrictions: .default,
                                                 summaryScreenVariant: summaryScreenVariant)
-        return .init(appName: "Proton Key",
+        return .init(appName: "Proton Pass",
                      clientApp: .drive,
                      doh: doh,
                      apiServiceDelegate: apiServiceDelegate,
