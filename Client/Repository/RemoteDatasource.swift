e45e215704d355fda67387819175527b4b48d7f6
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2022-08-05 13:08:52 +0200

    [PASSBE-143] Remote datasource able to fetch shares and share keys

---
+++ Client/Repository/RemoteDatasource.swift
@@ -0,0 +1,73 @@
+//
+// RemoteDatasource.swift
+// Proton Pass - Created on 05/08/2022.
+// Copyright (c) 2022 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+
+import ProtonCore_Networking
+import ProtonCore_Services
+
+public protocol RemoteDatasourceProtocol {
+    func getShares() async throws -> [Share]
+    func getShareKey(forShareId shareId: String) async throws -> ShareKey
+}
+
+public final class RemoteDatasource {
+    private let authCredential: AuthCredential
+    private let apiService: APIService
+
+    init(authCredential: AuthCredential,
+         apiService: APIService) {
+        self.authCredential = authCredential
+        self.apiService = apiService
+    }
+}
+
+extension RemoteDatasource: RemoteDatasourceProtocol {
+    public func getShares() async throws -> [Share] {
+        var shares = [Share]()
+
+        // Fetch the partial shares first
+        let getSharesEndpoint = GetSharesEndpoint(credential: authCredential)
+        let getSharesResponse = try await apiService.exec(endpoint: getSharesEndpoint)
+
+        // Then fetch full share for each partial share
+        try await withThrowingTaskGroup(of: Share.self) { [weak self] group in
+            guard let self = self else { return }
+            for partialShare in getSharesResponse.shares {
+                let getShareDataEndpoint = GetShareDataEndpoint(credential: authCredential,
+                                                                shareId: partialShare.shareID)
+                group.addTask {
+                    let getShareDataResponse =
+                    try await self.apiService.exec(endpoint: getShareDataEndpoint)
+                    return getShareDataResponse.share
+                }
+            }
+
+            for try await share in group { shares.append(share) }
+        }
+
+        return shares
+    }
+
+    public func getShareKey(forShareId shareId: String) async throws -> ShareKey {
+        let getShareKeysEndpoint = GetShareKeysEndpoint(credential: authCredential,
+                                                        shareId: shareId)
+        let getShareKeysResponse = try await apiService.exec(endpoint: getShareKeysEndpoint)
+        return getShareKeysResponse.keys
+    }
+}
