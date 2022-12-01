91bc0dcd99bbf70f3555cb61af8b25b605e2122d
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2022-12-01 13:28:23 +0100

    [IDTEAM-510] Able to copy note, email, password & alias of search results

--- iOS/Scenes/Home/Search/SearchView.swift
+++ iOS/Scenes/Home/Search/SearchView.swift
@@ -18,6 +18,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Proton Pass. If not, see https://www.gnu.org/licenses/.
 
+import Client
+import ProtonCore_UIFoundations
 import SwiftUI
 import UIComponents
 
@@ -73,11 +75,52 @@ struct SearchView: View {
     private var resultsList: some View {
         List {
             ForEach(viewModel.results, id: \.itemId) { result in
-                ItemSearchResultView(result: result, action: {})
+                ItemSearchResultView(result: result,
+                                     action: {},
+                                     trailingView: { trailingView(for: result) })
             }
         }
         .listStyle(.plain)
         .animation(.default, value: viewModel.results.count)
+    }
+
+    private func trailingView(for item: ItemSearchResult) -> some View {
+        Menu(content: {
+            switch item.type {
+            case .login:
+                CopyMenuButton(title: "Copy username",
+                               action: { viewModel.copyUsername(item) })
+
+                CopyMenuButton(title: "Copy password",
+                               action: { viewModel.copyPassword(item) })
+
+            case .alias:
+                CopyMenuButton(title: "Copy email address",
+                               action: { viewModel.copyEmailAddress(item) })
+            case .note:
+                if item.detail.first?.fullText.isEmpty == false {
+                    CopyMenuButton(title: "Copy note",
+                                   action: { viewModel.copyNote(item) })
+                }
+            }
+
+            EditMenuButton {
+                viewModel.editItem(item)
+            }
+
+            Divider()
+
+            DestructiveButton(
+                title: "Move to Trash",
+                icon: IconProvider.trash,
+                action: {
+//                    selectedItem = item
+//                    isShowingTrashingAlert.toggle()
+                })
+        }, label: {
+            Image(uiImage: IconProvider.threeDotsHorizontal)
+                .foregroundColor(.secondary)
+        })
     }
 }
 
