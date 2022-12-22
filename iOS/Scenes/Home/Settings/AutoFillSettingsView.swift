4b9b02a9b2f77a77e2eb7774cd80fbf67d8dcf7a
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2022-12-22 13:59:47 +0100

    [IDTEAM-661] Improve AutoFill settings

--- iOS/Scenes/Home/Settings/AutoFillSettingsView.swift
+++ iOS/Scenes/Home/Settings/AutoFillSettingsView.swift
@@ -20,6 +20,7 @@
 
 import ProtonCore_UIFoundations
 import SwiftUI
+import UIComponents
 
 struct AutoFillSettingsView: View {
     @ObservedObject var viewModel: SettingsViewModel
@@ -28,9 +29,12 @@ struct AutoFillSettingsView: View {
     var body: some View {
         Group {
             if viewModel.autoFillEnabled {
-                Text("Enabled")
+                AutoFillEnabledView(viewModel: viewModel)
             } else {
-                Text("Disabled")
+                AutoFillDisabledView()
+                    .padding()
+                    .background(OnboardingGradientBackground())
+                    .edgesIgnoringSafeArea(.all)
             }
         }
         .animation(.default, value: viewModel.autoFillEnabled)
@@ -50,6 +54,37 @@ struct AutoFillSettingsView: View {
 
         ToolbarItem(placement: .principal) {
             Text("AutoFill")
+        }
+    }
+}
+
+private struct AutoFillDisabledView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            OnboardingAutoFillView()
+            ColoredRoundedButton(title: "Go to Settings",
+                                 action: UIApplication.shared.openPasswordSettings)
+            .padding(.top)
+            Spacer()
+        }
+    }
+}
+
+private struct AutoFillEnabledView: View {
+    @ObservedObject var viewModel: SettingsViewModel
+
+    var body: some View {
+        Form {
+            Section(content: {
+                Toggle(isOn: $viewModel.quickTypeBar) {
+                    Text("QuickType bar suggestions")
+                }
+                .tint(.interactionNorm)
+            }, footer: {
+                // swiftlint:disable:next line_length
+                Text("QuickType bar helps quickly select a matched credential without opening the AutoFill extension.")
+            })
         }
     }
 }
