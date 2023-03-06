4fd4301ae286019f4034cf863c8bc41cd7c2d6ec
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-03-06 14:39:03 +0100

    [IDTEAM-835] Create new HomepageCoordinator

---
+++ iOS/Scenes/Homepage/HomepageCoordinator.swift
@@ -0,0 +1,81 @@
+//
+// HomepageCoordinator.swift
+// Proton Pass - Created on 06/03/2023.
+// Copyright (c) 2023 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+
+import Client
+import Core
+import SwiftUI
+import UIComponents
+import UIKit
+
+final class HomepageCoordinator: Coordinator, DeinitPrintable {
+    deinit { print(deinitMessage) }
+
+    private let clipboardManager: ClipboardManager
+    private let credentialManager: CredentialManagerProtocol
+    private let logManager: LogManager
+    private let preferences: Preferences
+
+    // Lazily initialized properties
+    private lazy var bannerManager: BannerManager = { .init(container: rootViewController) }()
+
+    init(credentialManager: CredentialManagerProtocol,
+         logManager: LogManager,
+         preferences: Preferences) {
+        self.clipboardManager = .init(preferences: preferences)
+        self.credentialManager = credentialManager
+        self.logManager = logManager
+        self.preferences = preferences
+        super.init()
+        self.finalizeInitialization()
+        self.start()
+    }
+}
+
+// MARK: - Private APIs
+private extension HomepageCoordinator {
+    /// Some properties are dependant on other propeties which are in turn not initialized
+    /// before the Coordinator is fully initialized. This method is to resolve these dependencies.
+    func finalizeInitialization() {
+        clipboardManager.bannerManager = bannerManager
+    }
+
+    func start() {
+        let placeholderView = ItemDetailPlaceholderView {
+            self.popTopViewController(animated: true)
+        }
+        start(with: HomepageView(), secondaryView: placeholderView)
+    }
+}
+
+// MARK: - Public APIs
+extension HomepageCoordinator {
+    func onboardIfNecessary() {
+        guard !preferences.onboarded else { return }
+        let onboardingViewModel = OnboardingViewModel(credentialManager: credentialManager,
+                                                      preferences: preferences,
+                                                      bannerManager: bannerManager,
+                                                      logManager: logManager)
+        let onboardingView = OnboardingView(viewModel: onboardingViewModel)
+        let onboardingViewController = UIHostingController(rootView: onboardingView)
+        onboardingViewController.modalPresentationStyle = UIDevice.current.isIpad ? .formSheet : .fullScreen
+        onboardingViewController.isModalInPresentation = true
+        topMostViewController.present(onboardingViewController, animated: true)
+    }
+}
