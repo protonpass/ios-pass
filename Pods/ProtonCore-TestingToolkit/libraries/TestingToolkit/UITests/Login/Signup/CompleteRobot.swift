e62a9ea9d4241288ea6ac08cd07faa1db78400a4
Krzysztof Siejkowski <krzysztof.siejkowski@proton.ch>
2023-03-14 11:58:14 +0100

    [CP-5339] Update core to 3.29.0

--- Pods/ProtonCore-TestingToolkit/libraries/TestingToolkit/UITests/Login/Signup/CompleteRobot.swift
+++ Pods/ProtonCore-TestingToolkit/libraries/TestingToolkit/UITests/Login/Signup/CompleteRobot.swift
@@ -20,7 +20,7 @@
 //  along with ProtonCore.  If not, see <https://www.gnu.org/licenses/>.
 
 import Foundation
-import pmtest
+import fusion
 
 private let titleId = "CompleteViewController.completeTitleLabel"
 private let descriptionId = "CompleteViewController.completeDescriptionLabel"
