2cdd3adff280bdd35600ea2c94a1e43c3c682b21
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-03-30 14:05:52 +0200

    [IDTEAM-1037] Simplify RepositoryManager

--- Client/Repositories/RepositoryManager.swift
+++ Client/Repositories/RepositoryManager.swift
@@ -27,148 +27,68 @@ import ProtonCore_Services
 public final class RepositoryManager: DeinitPrintable {
     deinit { print(deinitMessage) }
 
-    private let apiService: APIService
-    private let container: NSPersistentContainer
-    private let logManager: LogManager
-    private let symmetricKey: SymmetricKey
-    private let userData: UserData
+    public let aliasRepository: AliasRepositoryProtocol
+    public let itemRepository: ItemRepositoryProtocol
+    public let passKeyManager: PassKeyManagerProtocol
+    public let shareEventIDRepository: ShareEventIDRepositoryProtocol
+    public let shareRepository: ShareRepositoryProtocol
+    public let shareKeyRepository: ShareKeyRepositoryProtocol
+
+    public let localSearchEntryDatasource: LocalSearchEntryDatasourceProtocol
+    public let remoteSyncEventsDatasource: RemoteSyncEventsDatasourceProtocol
 
     public init(apiService: APIService,
                 container: NSPersistentContainer,
                 logManager: LogManager,
                 symmetricKey: SymmetricKey,
                 userData: UserData) {
-        self.apiService = apiService
-        self.container = container
-        self.logManager = logManager
-        self.symmetricKey = symmetricKey
-        self.userData = userData
-    }
+        let remoteAliasDatasource = RemoteAliasDatasource(apiService: apiService)
+        let localItemDatasource = LocalItemDatasource(container: container)
+        let remoteItemDatasource = RemoteItemRevisionDatasource(apiService: apiService)
+        let localShareDatasource = LocalShareDatasource(container: container)
+        let remoteShareDatasource = RemoteShareDatasource(apiService: apiService)
+        let localShareKeyDatasource = LocalShareKeyDatasource(container: container)
+        let remoteShareKeyDatasource = RemoteShareKeyDatasource(apiService: apiService)
+        let remoteItemKeyDatasource = RemoteItemKeyDatasource(apiService: apiService)
+        let localShareEventIDDatasource = LocalShareEventIDDatasource(container: container)
+        let remoteShareEventIDDatasource = RemoteShareEventIDDatasource(apiService: apiService)
 
-    // Repositories
-    public lazy var aliasRepository = makeAliasRepository()
-    public lazy var itemRepository = makeItemRepository()
-    public lazy var passKeyManager = makePassKeyManager()
-    public lazy var shareEventIDRepository = makeShareEventIDRepository()
-    public lazy var shareRepository = makeShareRespository()
-    public lazy var shareKeyRepository = makeShareKeyRepository()
+        let shareKeyRepository = ShareKeyRepository(localShareKeyDatasource: localShareKeyDatasource,
+                                                    remoteShareKeyDatasource: remoteShareKeyDatasource,
+                                                    logManager: logManager,
+                                                    userData: userData)
 
-    // Public datasources
-    public lazy var localSearchEntryDatasource = makeLocalSearchEntryDatasource()
-    public lazy var remoteSyncEventsDatasource = makeRemoteSyncEventsDatasource()
+        let passKeyManager = PassKeyManager(userData: userData,
+                                            shareKeyRepository: shareKeyRepository,
+                                            itemKeyDatasource: remoteItemKeyDatasource,
+                                            logManager: logManager)
 
-    // Private datasources
-    private lazy var remoteAliasDatasource = makeRemoteAliasDatasource()
-
-    private lazy var localItemDatasource = makeLocalItemDatasource()
-    private lazy var remoteItemDatasource = makeRemoteItemDatasource()
-
-    private lazy var localShareDatasource = makeLocalShareDatasource()
-    private lazy var remoteShareDatasource = makeRemoteShareDatasource()
-
-    private lazy var localShareKeyDatasource = makeLocalShareKeyDatasource()
-    private lazy var remoteShareKeyDatasource = makeRemoteShareKeyDatasource()
-
-    private lazy var remoteItemKeyDatasource = makeRemoteItemKeyDatasource()
-
-    private lazy var localShareEventIDDatasource = makeLocalShareEventIDDatasource()
-    private lazy var remoteShareEventIDDatasource = makeRemoteShareEventIDDatasource()
-}
-
-// MARK: - Make repositories
-private extension RepositoryManager {
-    func makeAliasRepository() -> AliasRepositoryProtocol {
-        AliasRepository(remoteAliasDatasouce: remoteAliasDatasource)
-    }
-
-    func makeItemRepository() -> ItemRepositoryProtocol {
-        ItemRepository(userData: userData,
-                       symmetricKey: symmetricKey,
-                       localItemDatasoure: localItemDatasource,
-                       remoteItemRevisionDatasource: remoteItemDatasource,
-                       shareRepository: shareRepository,
-                       shareEventIDRepository: shareEventIDRepository,
-                       passKeyManager: passKeyManager,
-                       logManager: logManager)
-    }
-
-    func makePassKeyManager() -> PassKeyManagerProtocol {
-        PassKeyManager(userData: userData,
-                       shareKeyRepository: shareKeyRepository,
-                       itemKeyDatasource: remoteItemKeyDatasource,
-                       logManager: logManager)
-    }
-
-    func makeShareRespository() -> ShareRepositoryProtocol {
-        ShareRepository(userData: userData,
-                        localShareDatasource: localShareDatasource,
-                        remoteShareDatasouce: remoteShareDatasource,
-                        passKeyManager: passKeyManager,
-                        logManager: logManager)
-    }
-
-    func makeShareEventIDRepository() -> ShareEventIDRepositoryProtocol {
-        ShareEventIDRepository(
+        let shareEventIDRepository = ShareEventIDRepository(
             localShareEventIDDatasource: localShareEventIDDatasource,
             remoteShareEventIDDatasource: remoteShareEventIDDatasource,
             logManager: logManager)
-    }
 
-    func makeShareKeyRepository() -> ShareKeyRepositoryProtocol {
-        ShareKeyRepository(localShareKeyDatasource: localShareKeyDatasource,
-                           remoteShareKeyDatasource: remoteShareKeyDatasource,
-                           logManager: logManager,
-                           userData: userData)
-    }
-}
+        let shareRepository = ShareRepository(userData: userData,
+                                              localShareDatasource: localShareDatasource,
+                                              remoteShareDatasouce: remoteShareDatasource,
+                                              passKeyManager: passKeyManager,
+                                              logManager: logManager)
 
-// MARK: - Make datasources
-private extension RepositoryManager {
-    func makeRemoteAliasDatasource() -> RemoteAliasDatasourceProtocol {
-        RemoteAliasDatasource(apiService: apiService)
-    }
+        self.aliasRepository = AliasRepository(remoteAliasDatasouce: remoteAliasDatasource)
+        self.itemRepository = ItemRepository(userData: userData,
+                                             symmetricKey: symmetricKey,
+                                             localItemDatasoure: localItemDatasource,
+                                             remoteItemRevisionDatasource: remoteItemDatasource,
+                                             shareRepository: shareRepository,
+                                             shareEventIDRepository: shareEventIDRepository,
+                                             passKeyManager: passKeyManager,
+                                             logManager: logManager)
+        self.passKeyManager = passKeyManager
+        self.shareEventIDRepository = shareEventIDRepository
+        self.shareRepository = shareRepository
+        self.shareKeyRepository = shareKeyRepository
 
-    func makeLocalItemDatasource() -> LocalItemDatasourceProtocol {
-        LocalItemDatasource(container: container)
-    }
-
-    func makeRemoteItemDatasource() -> RemoteItemRevisionDatasourceProtocol {
-        RemoteItemRevisionDatasource(apiService: apiService)
-    }
-
-    func makeLocalShareDatasource() -> LocalShareDatasourceProtocol {
-        LocalShareDatasource(container: container)
-    }
-
-    func makeRemoteShareDatasource() -> RemoteShareDatasourceProtocol {
-        RemoteShareDatasource(apiService: apiService)
-    }
-
-    func makeLocalShareKeyDatasource() -> LocalShareKeyDatasourceProtocol {
-        LocalShareKeyDatasource(container: container)
-    }
-
-    func makeRemoteShareKeyDatasource() -> RemoteShareKeyDatasourceProtocol {
-        RemoteShareKeyDatasource(apiService: apiService)
-    }
-
-    func makeRemoteItemKeyDatasource() -> RemoteItemKeyDatasourceProtocol {
-        RemoteItemKeyDatasource(apiService: apiService)
-    }
-
-    func makeLocalShareEventIDDatasource() -> LocalShareEventIDDatasourceProtocol {
-        LocalShareEventIDDatasource(container: container)
-    }
-
-    func makeRemoteShareEventIDDatasource() -> RemoteShareEventIDDatasourceProtocol {
-        RemoteShareEventIDDatasource(apiService: apiService)
-    }
-
-    func makeLocalSearchEntryDatasource() -> LocalSearchEntryDatasourceProtocol {
-        LocalSearchEntryDatasource(container: container)
-    }
-
-    func makeRemoteSyncEventsDatasource() -> RemoteSyncEventsDatasourceProtocol {
-        RemoteSyncEventsDatasource(apiService: apiService)
+        self.localSearchEntryDatasource = LocalSearchEntryDatasource(container: container)
+        self.remoteSyncEventsDatasource = RemoteSyncEventsDatasource(apiService: apiService)
     }
 }
