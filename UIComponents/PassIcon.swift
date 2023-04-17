d6978461df31a944a0ba5e0ffabc3c1d3d75a8ff
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-04-17 15:05:32 +0200

    fix(onboarding): remove unused icons & test icons

--- UIComponents/PassIcon.swift
+++ UIComponents/PassIcon.swift
@@ -18,64 +18,62 @@
 // You should have received a copy of the GNU General Public License
 // along with Proton Pass. If not, see https://www.gnu.org/licenses/.
 
+// swiftlint:disable force_unwrapping
 import UIKit
 
-// swiftlint:disable force_unwrapping
-public enum PassIcon {
-    public static var brandPass = UIImage(named: "BrandPass")!
-    public static var passIcon = UIImage(named: "ProtonPassIcon")!
-    public static var passTextLogo = UIImage(named: "PassTextLogo")!
-    public static var swirls = UIImage(named: "Swirls")!
-    public static var trash = UIImage(named: "Trash")!
-    public static var magnifyingGlass = UIImage(named: "MagnifyingGlass")!
-    public static var emptyFolder = UIImage(named: "EmptyFolder")!
-    public static var onboardAutoFillGradient = UIImage(named: "OnboardAutoFillGradient")!
-    public static var onboardAutoFillEnabled = UIImage(named: "OnboardAutoFillEnabled")!
-    public static var onboardAutoFillStep1 = UIImage(named: "OnboardAutoFillStep1")!
-    public static var onboardAutoFillStep2 = UIImage(named: "OnboardAutoFillStep2")!
-    public static var onboardAutoFillStep3 = UIImage(named: "OnboardAutoFillStep3")!
-    public static var onboardAutoFillStep4 = UIImage(named: "OnboardAutoFillStep4")!
-    public static var onboardAutoFillStep5 = UIImage(named: "OnboardAutoFillStep5")!
-    public static var onboardBiometricAuthentication = UIImage(named: "OnboardBiometricAuthentication")!
-    // swiftlint:disable:next line_length
-    public static var onboardBiometricAuthenticationBackground = UIImage(named: "OnboardBiometricAuthenticationBackground")!
-    // swiftlint:disable:next line_length
-    public static var onboardBiometricAuthenticationEnabled = UIImage(named: "OnboardBiometricAuthenticationEnabled")!
-    public static var onboardAliases = UIImage(named: "OnboardAliases")!
-    public static var turnOnAutoFillBannerGradient = UIImage(named: "TurnOnAutoFillBannerGradient")!
-    public static var turnOnAutoFillBannerKeyboard = UIImage(named: "TurnOnAutoFillBannerKeyboard")!
-    public static var brandTwitter = UIImage(named: "BrandTwitter")!
-    public static var brandReddit = UIImage(named: "BrandReddit")!
+public enum PassIcon {}
 
-    public static var allVaults = UIImage(named: "AllVaults")!
-    public static var vaultIcon1 = UIImage(named: "VaultIcon1")!
-    public static var vaultIcon2 = UIImage(named: "VaultIcon2")!
-    public static var vaultIcon3 = UIImage(named: "VaultIcon3")!
-    public static var vaultIcon4 = UIImage(named: "VaultIcon4")!
-    public static var vaultIcon5 = UIImage(named: "VaultIcon5")!
-    public static var vaultIcon6 = UIImage(named: "VaultIcon6")!
-    public static var vaultIcon7 = UIImage(named: "VaultIcon7")!
-    public static var vaultIcon8 = UIImage(named: "VaultIcon8")!
-    public static var vaultIcon9 = UIImage(named: "VaultIcon9")!
-    public static var vaultIcon10 = UIImage(named: "VaultIcon10")!
-    public static var vaultIcon11 = UIImage(named: "VaultIcon11")!
-    public static var vaultIcon12 = UIImage(named: "VaultIcon12")!
-    public static var vaultIcon13 = UIImage(named: "VaultIcon13")!
-    public static var vaultIcon14 = UIImage(named: "VaultIcon14")!
-    public static var vaultIcon15 = UIImage(named: "VaultIcon15")!
-    public static var vaultIcon16 = UIImage(named: "VaultIcon16")!
-    public static var vaultIcon17 = UIImage(named: "VaultIcon17")!
-    public static var vaultIcon18 = UIImage(named: "VaultIcon18")!
-    public static var vaultIcon19 = UIImage(named: "VaultIcon19")!
-    public static var vaultIcon20 = UIImage(named: "VaultIcon20")!
-    public static var vaultIcon21 = UIImage(named: "VaultIcon21")!
-    public static var vaultIcon22 = UIImage(named: "VaultIcon22")!
-    public static var vaultIcon23 = UIImage(named: "VaultIcon23")!
-    public static var vaultIcon24 = UIImage(named: "VaultIcon24")!
-    public static var vaultIcon25 = UIImage(named: "VaultIcon25")!
-    public static var vaultIcon26 = UIImage(named: "VaultIcon26")!
-    public static var vaultIcon27 = UIImage(named: "VaultIcon27")!
-    public static var vaultIcon28 = UIImage(named: "VaultIcon28")!
-    public static var vaultIcon29 = UIImage(named: "VaultIcon29")!
-    public static var vaultIcon30 = UIImage(named: "VaultIcon30")!
+public extension PassIcon {
+    static var brandPass = UIImage(named: "BrandPass")!
+    static var brandReddit = UIImage(named: "BrandReddit")!
+    static var brandTwitter = UIImage(named: "BrandTwitter")!
+    static var magnifyingGlass = UIImage(named: "MagnifyingGlass")!
+
+    static var onboardAliases = UIImage(named: "OnboardAliases")!
+    static var onboardAuthentication = UIImage(named: "OnboardAuthentication")!
+    static var onboardAuthenticationBackground = UIImage(named: "OnboardAuthenticationBackground")!
+    static var onboardAuthenticationEnabled = UIImage(named: "OnboardAuthenticationEnabled")!
+    static var onboardAutoFillEnabled = UIImage(named: "OnboardAutoFillEnabled")!
+    static var onboardAutoFillStep1 = UIImage(named: "OnboardAutoFillStep1")!
+    static var onboardAutoFillStep2 = UIImage(named: "OnboardAutoFillStep2")!
+    static var onboardAutoFillStep3 = UIImage(named: "OnboardAutoFillStep3")!
+    static var onboardAutoFillStep4 = UIImage(named: "OnboardAutoFillStep4")!
+    static var onboardAutoFillStep5 = UIImage(named: "OnboardAutoFillStep5")!
+
+    static var passIcon = UIImage(named: "PassIcon")!
+    static var passTextLogo = UIImage(named: "PassTextLogo")!
+    static var swirls = UIImage(named: "Swirls")!
+    static var trash = UIImage(named: "Trash")!
+
+    static var allVaults = UIImage(named: "AllVaults")!
+    static var vaultIcon1 = UIImage(named: "VaultIcon1")!
+    static var vaultIcon2 = UIImage(named: "VaultIcon2")!
+    static var vaultIcon3 = UIImage(named: "VaultIcon3")!
+    static var vaultIcon4 = UIImage(named: "VaultIcon4")!
+    static var vaultIcon5 = UIImage(named: "VaultIcon5")!
+    static var vaultIcon6 = UIImage(named: "VaultIcon6")!
+    static var vaultIcon7 = UIImage(named: "VaultIcon7")!
+    static var vaultIcon8 = UIImage(named: "VaultIcon8")!
+    static var vaultIcon9 = UIImage(named: "VaultIcon9")!
+    static var vaultIcon10 = UIImage(named: "VaultIcon10")!
+    static var vaultIcon11 = UIImage(named: "VaultIcon11")!
+    static var vaultIcon12 = UIImage(named: "VaultIcon12")!
+    static var vaultIcon13 = UIImage(named: "VaultIcon13")!
+    static var vaultIcon14 = UIImage(named: "VaultIcon14")!
+    static var vaultIcon15 = UIImage(named: "VaultIcon15")!
+    static var vaultIcon16 = UIImage(named: "VaultIcon16")!
+    static var vaultIcon17 = UIImage(named: "VaultIcon17")!
+    static var vaultIcon18 = UIImage(named: "VaultIcon18")!
+    static var vaultIcon19 = UIImage(named: "VaultIcon19")!
+    static var vaultIcon20 = UIImage(named: "VaultIcon20")!
+    static var vaultIcon21 = UIImage(named: "VaultIcon21")!
+    static var vaultIcon22 = UIImage(named: "VaultIcon22")!
+    static var vaultIcon23 = UIImage(named: "VaultIcon23")!
+    static var vaultIcon24 = UIImage(named: "VaultIcon24")!
+    static var vaultIcon25 = UIImage(named: "VaultIcon25")!
+    static var vaultIcon26 = UIImage(named: "VaultIcon26")!
+    static var vaultIcon27 = UIImage(named: "VaultIcon27")!
+    static var vaultIcon28 = UIImage(named: "VaultIcon28")!
+    static var vaultIcon29 = UIImage(named: "VaultIcon29")!
+    static var vaultIcon30 = UIImage(named: "VaultIcon30")!
 }
