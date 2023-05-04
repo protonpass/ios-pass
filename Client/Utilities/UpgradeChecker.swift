accc5f49c93d9a54b7d08927a1bc45fd47dd644c
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-05-04 21:35:58 +0200

    feat(user-access): implement UpgradeChecker that helps check for upgrade

---
+++ Client/Utilities/UpgradeChecker.swift
@@ -0,0 +1,71 @@
+//
+// UpgradeChecker.swift
+// Proton Pass - Created on 04/05/2023.
+// Copyright (c) 2023 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+
+import Core
+
+public protocol UpgradeCheckerProtocol: AnyObject {
+    var passPlanRepository: PassPlanRepositoryProtocol { get }
+    var counter: LimitationCounterProtocol { get }
+
+    func canCreateMoreVaults() async throws -> Bool
+    func canCreateMoreTOTPs() async throws -> Bool
+    func isFreeUser() async throws -> Bool
+}
+
+public extension UpgradeCheckerProtocol {
+    func canCreateMoreVaults() async throws -> Bool {
+        let plan = try await passPlanRepository.getPlan()
+        if let vaultLimit = plan.vaultLimit {
+            let vaultCount = counter.getVaultCount()
+            return vaultCount < vaultLimit
+        }
+        return true
+    }
+
+    func canCreateMoreTOTPs() async throws -> Bool {
+        let plan = try await passPlanRepository.getPlan()
+        if let totpLimit = plan.totpLimit {
+            let totpCount = counter.getTOTPCount()
+            return totpCount < totpLimit
+        }
+        return true
+    }
+
+    func isFreeUser() async throws -> Bool {
+        let plan = try await passPlanRepository.getPlan()
+        return plan.planType == .free
+    }
+}
+
+public protocol LimitationCounterProtocol {
+    func getVaultCount() -> Int
+    func getTOTPCount() -> Int
+}
+
+public final class UpgradeChecker: UpgradeCheckerProtocol {
+    public let passPlanRepository: PassPlanRepositoryProtocol
+    public let counter: LimitationCounterProtocol
+
+    public init(passPlanRepository: PassPlanRepositoryProtocol,
+                counter: LimitationCounterProtocol) {
+        self.passPlanRepository = passPlanRepository
+        self.counter = counter
+    }
+}
