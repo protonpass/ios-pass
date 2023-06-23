c35e87bf95c81d02061be12c98b4d8c5a9cc8be7
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-06-23 16:24:36 +0200

    ci: add exit code and update gitlab-ci.yml

--- scripts/swiftformat.sh
+++ scripts/swiftformat.sh
@@ -10,7 +10,9 @@ exit_code=$?
 if [ $exit_code -eq 0 ]; then
 # Print in green success message
   echo "\033[32;1mCode is properly formatted.\033[0m"
+  exit 0
 else
 # Print in red failure message
   echo "\033[1;31mCode is not properly formatted. Please ensure that you have swiftformat installed.\033[0m"
+  exit 1
 fi
