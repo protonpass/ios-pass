7a4ad6a8d02f5c7234332c884b913927d7fb8f0f
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-06-23 16:16:39 +0200

    ci: add swiftformat check script

---
+++ scripts/swiftformat.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check if code is properly formatted or not
+swiftformat --lint .
+
+# Capture the exit code of the previous command
+exit_code=$?
+
+# Check the exit code
+if [ $exit_code -eq 0 ]; then
+# Print in green success message
+  echo "\033[32;1mCode is properly formatted.\033[0m"
+else
+# Print in red failure message
+  echo "\033[1;31mCode is not properly formatted. Please ensure that you have swiftformat installed.\033[0m"
+fi
