4ccefcd74a2cac73755665d5299c33f8fbf196c4
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-07-08 22:54:13 +0200

    refactor(di): add convenient functions to Factory

---
+++ iOS/AppManagement/DependencyInjection/Factory+Extensions.swift
@@ -0,0 +1,44 @@
+//
+// Factory+Extensions.swift
+// Proton Pass - Created on 08/07/2023.
+// Copyright (c) 2023 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+
+import Factory
+
+extension FactoryContext {
+    /// Overload of `setArg(_:forKey:)`  to make it more flexible
+    /// by taking `arg` as `any RawRepresentable<String>` instead of `String`
+    static func setArg(_ arg: any RawRepresentable<String>, forKey key: String) {
+        setArg(arg.rawValue, forKey: key)
+    }
+
+    /// Overload of `removeArg(forKey:)`  to make it more flexible
+    /// by taking `key` as `any RawRepresentable<String>` instead of `String`
+    static func removeArg(forKey key: any RawRepresentable<String>) {
+        removeArg(forKey: key.rawValue)
+    }
+}
+
+extension FactoryModifying {
+    /// Overload of `onArg(_:factory:)`  to make it more flexible
+    /// by taking `arg` as `any RawRepresentable<String>` instead of `String`
+    @discardableResult
+    func onArg(_ arg: any RawRepresentable<String>, factory: @escaping (P) -> T) -> Self {
+        onArg(arg.rawValue, factory: factory)
+    }
+}
