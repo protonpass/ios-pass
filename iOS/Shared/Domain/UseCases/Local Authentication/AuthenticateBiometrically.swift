35f761f7bfe4472e89703a63623b2a27bae135d4
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-07-13 16:49:01 +0200

    feat(pin): add use cases for biometric authentication

---
+++ iOS/Shared/Domain/UseCases/Local Authentication/AuthenticateBiometrically.swift
@@ -0,0 +1,40 @@
+//
+// AuthenticateBiometrically.swift
+// Proton Pass - Created on 13/07/2023.
+// Copyright (c) 2023 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+
+import LocalAuthentication
+
+/// Biometrically authenticate with a given reason
+protocol AuthenticateBiometricallyUseCase: Sendable {
+    func execute(for policy: LAPolicy, context: LAContext, reason: String) async throws -> Bool
+}
+
+extension AuthenticateBiometricallyUseCase {
+    func callAsFunction(for policy: LAPolicy, context: LAContext, reason: String) async throws -> Bool {
+        try await execute(for: policy, context: context, reason: reason)
+    }
+}
+
+final class AuthenticateBiometrically: AuthenticateBiometricallyUseCase {
+    init() {}
+
+    func execute(for policy: LAPolicy, context: LAContext, reason: String) async throws -> Bool {
+        try await context.evaluatePolicy(policy, localizedReason: reason)
+    }
+}
