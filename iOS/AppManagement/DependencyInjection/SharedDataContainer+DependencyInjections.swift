98f1ab294bd207afaa4fc90866bf1c43ad341829
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-07-25 11:01:05 +0200

    refactor(di): add SharedDataContainer

---
+++ iOS/AppManagement/DependencyInjection/SharedDataContainer+DependencyInjections.swift
@@ -0,0 +1,58 @@
+//
+// SharedDataContainer+DependencyInjections.swift
+// Proton Pass - Created on 25/07/2023.
+// Copyright (c) 2023 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+
+import Client
+import Core
+import CoreData
+import CryptoKit
+import Factory
+import Foundation
+import ProtonCore_Login
+
+final class SharedDataContainer: SharedContainer, AutoRegistering {
+    static let shared = SharedDataContainer()
+    let manager = ContainerManager()
+
+    func autoRegister() {
+        manager.defaultScope = .cached
+    }
+
+    func resolve(container: NSPersistentContainer,
+                 symmetricKey: SymmetricKey,
+                 userData: UserData) {
+        self.container.register { container }
+        self.symmetricKey.register { symmetricKey }
+        self.userData.register { userData }
+    }
+}
+
+extension SharedDataContainer {
+    var container: Factory<NSPersistentContainer> {
+        self { fatalError("container not registered") }
+    }
+
+    var symmetricKey: Factory<SymmetricKey> {
+        self { fatalError("symmetricKey not registered") }
+    }
+
+    var userData: Factory<UserData> {
+        self { fatalError("userData not registered") }
+    }
+}
