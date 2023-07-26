3d95907e709bdb213703c3a7ffa3d5a7d188775e
Martin Lukacs <martin.lukacs@proton.ch>
2023-07-26 16:24:05 +0200

    fix offset sheet bug from homepage after search

---
+++ UIComponents/ViewModifiers/SyncLayoutOnDisappear.swift
@@ -0,0 +1,36 @@
+//
+// SyncLayoutOnDisappear.swift
+// Proton Pass - Created on 26/07/2023.
+// Copyright (c) 2023 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+
+import SwiftUI
+
+public struct SyncLayoutOnDisappear: ViewModifier {
+    public init() {}
+
+    public func body(content: Content) -> some View {
+        content
+            .onDisappear {
+                let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+                if let viewFrame = scene?.windows.first?.rootViewController?.view.frame {
+                    scene?.windows.first?.rootViewController?.view.frame = .zero
+                    scene?.windows.first?.rootViewController?.view.frame = viewFrame
+                }
+            }
+    }
+}
