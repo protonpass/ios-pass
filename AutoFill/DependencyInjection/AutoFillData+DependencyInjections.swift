f2fbf5088a8abe352c9bf130ba23b284c6f5ab7c
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-07-31 15:17:55 +0200

    refactor(autofill): inject ASCredentialProviderExtensionContext

---
+++ AutoFill/DependencyInjection/AutoFillData+DependencyInjections.swift
@@ -0,0 +1,42 @@
+//
+// AutoFillData+DependencyInjections.swift
+// Proton Pass - Created on 31/07/2023.
+// Copyright (c) 2023 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+
+import AuthenticationServices
+import Factory
+import UIKit
+
+final class AutoFillDataContainer: SharedContainer, AutoRegistering {
+    static let shared = AutoFillDataContainer()
+    let manager = ContainerManager()
+
+    func autoRegister() {
+        manager.defaultScope = .shared
+    }
+
+    func register(context: ASCredentialProviderExtensionContext) {
+        self.context.register { context }
+    }
+}
+
+extension AutoFillDataContainer {
+    var context: Factory<ASCredentialProviderExtensionContext> {
+        self { fatalError("context not registered") }
+    }
+}
