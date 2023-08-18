df95d917d4febd13ff98013e7054e925cc0e7a4b
Martin Lukacs <martin.lukacs@proton.ch>
2023-08-18 17:50:50 +0200

    beginning of banner & invite merge

--- iOS/Scenes/Homepage/Items Tab/InfoBanner/InfoBannerView.swift
+++ iOS/Scenes/Homepage/Items Tab/InfoBanner/InfoBannerView.swift
@@ -33,11 +33,19 @@ struct InfoBannerView: View {
             informationDisplayView
                 .padding(.horizontal, 25)
                 .padding(.vertical, 5)
-            closeButtonView
+            if !banner.isInvite {
+                closeButtonView
+            }
         }
         .frame(height: Self.height)
-        .background(Color(uiColor: banner.detail.backgroundColor))
+        .background(banner.detail.backgroundColor)
         .clipShape(RoundedRectangle(cornerRadius: 16))
+        .onTapGesture {
+            guard banner.isInvite else {
+                return
+            }
+            action()
+        }
     }
 }
 
@@ -60,7 +68,7 @@ private extension InfoBannerView {
                 }
                 Spacer()
             }
-            .foregroundColor(PassColor.textInvert.toColor)
+            .foregroundColor(banner.detail.forgroundColor)
 
             Spacer()
 
