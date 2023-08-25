0f97c8d9b2dd5af49e1147d3df06ad79c00c402a
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-08-25 16:14:04 +0200

    l10n: localize create/edit alias page

--- iOS/Scenes/Homepage/Create & Edit Items/CreateAliasLiteView.swift
+++ iOS/Scenes/Homepage/Create & Edit Items/CreateAliasLiteView.swift
@@ -23,6 +23,7 @@ import ProtonCore_UIFoundations
 import SwiftUI
 import UIComponents
 
+// Localized
 struct CreateAliasLiteView: View {
     @Environment(\.dismiss) private var dismiss
     @StateObject private var viewModel: CreateAliasLiteViewModel
@@ -121,14 +122,14 @@ struct CreateAliasLiteView: View {
 
     private var buttons: some View {
         HStack(spacing: 16) {
-            CapsuleTextButton(title: "Cancel",
+            CapsuleTextButton(title: "Cancel".localized,
                               titleColor: PassColor.textWeak,
                               backgroundColor: PassColor.textDisabled,
                               height: 44,
                               action: dismiss.callAsFunction)
 
             if viewModel.canCreateAlias {
-                DisablableCapsuleTextButton(title: "Confirm",
+                DisablableCapsuleTextButton(title: "Confirm".localized,
                                             titleColor: PassColor.textInvert,
                                             disableTitleColor: PassColor.textHint,
                                             backgroundColor: PassColor.loginInteractionNormMajor1,
