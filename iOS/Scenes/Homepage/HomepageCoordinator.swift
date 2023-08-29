3a5f9cba0a1db7618dcd754d8859a2aeb45a6e35
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-08-29 11:04:49 +0200

    refactor: use router instead of delegate

--- iOS/Scenes/Homepage/HomepageCoordinator.swift
+++ iOS/Scenes/Homepage/HomepageCoordinator.swift
@@ -264,6 +264,8 @@ private extension HomepageCoordinator {
                     self.presentMailboxSelectionView(selection: mailboxSelection,
                                                      mode: .createAliasLite,
                                                      titleMode: mode)
+                case .autoFillInstructions:
+                    self.present(AutoFillInstructionsView())
                 }
             }
             .store(in: &cancellables)
@@ -746,10 +748,6 @@ extension HomepageCoordinator: CreateEditItemCoordinatorDelegate {
 // MARK: - ProfileTabViewModelDelegate
 
 extension HomepageCoordinator: ProfileTabViewModelDelegate {
-    func profileTabViewModelWantsToShowEnableAutoFillInstructions() {
-        showView(view: AutoFillInstructionsView(), asSheet: true)
-    }
-
     func profileTabViewModelWantsToShowAccountMenu() {
         let asSheet = shouldShowAsSheet()
         let viewModel = AccountViewModel(isShownAsSheet: asSheet)
