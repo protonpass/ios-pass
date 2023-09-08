223f350aad6d6a3ce123dd43fcfd13c50299f9e1
Martin Lukacs <martin.lukacs@proton.ch>
2023-09-08 15:53:32 +0200

    finished transfer alert

--- iOS/Shared/Domain/UseCases/Sharing+Invitations/GetAllUsersForShare.swift
+++ iOS/Shared/Domain/UseCases/Sharing+Invitations/GetAllUsersForShare.swift
@@ -50,12 +50,16 @@ final class GetAllUsersForShare: GetAllUsersForShareUseCase {
     }
 }
 
-struct ShareUser: Equatable, Hashable {
+struct ShareUser: Equatable, Hashable, Identifiable {
     let email: String
     let shareRole: ShareRole?
     let inviteID: String?
     let shareID: String?
     let userName: String?
+
+    var id: Int {
+        hashValue
+    }
 
     var isPending: Bool {
         shareRole == nil
