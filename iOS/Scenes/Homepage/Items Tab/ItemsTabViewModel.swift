dd7e7165976df8f54a23359be3009920d984dc0f
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-09-14 12:26:15 +0200

    feat(sync-progress): integrate sync progress to homepage

--- iOS/Scenes/Homepage/Items Tab/ItemsTabViewModel.swift
+++ iOS/Scenes/Homepage/Items Tab/ItemsTabViewModel.swift
@@ -42,6 +42,7 @@ final class ItemsTabViewModel: ObservableObject, PullToRefreshable, DeinitPrinta
     var selectedSortType = SortType.mostRecent
 
     @Published private(set) var banners: [InfoBanner] = []
+    @Published var shouldShowSyncProgress = false
     @Published var itemToBePermanentlyDeleted: ItemTypeIdentifiable? {
         didSet {
             if itemToBePermanentlyDeleted != nil {
@@ -57,6 +58,7 @@ final class ItemsTabViewModel: ObservableObject, PullToRefreshable, DeinitPrinta
     private let credentialManager = resolve(\SharedServiceContainer.credentialManager)
     private let logger = resolve(\SharedToolingContainer.logger)
     private let preferences = resolve(\SharedToolingContainer.preferences)
+    private let manualLogIn = resolve(\SharedDataContainer.manualLogIn)
     private let getPendingUserInvitations = resolve(\UseCasesContainer.getPendingUserInvitations)
     let vaultsManager = resolve(\SharedServiceContainer.vaultsManager)
     let itemContextMenuHandler = resolve(\SharedServiceContainer.itemContextMenuHandler)
@@ -94,6 +96,15 @@ private extension ItemsTabViewModel {
                 self?.refreshBanners()
             }
             .store(in: &cancellables)
+
+        // Show the progress if after 5 seconds after logging in and items are not yet loaded
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) { [weak self] in
+            guard let self else { return }
+            if self.manualLogIn,
+               case .loading = self.vaultsManager.state {
+                self.shouldShowSyncProgress = true
+            }
+        }
     }
 
     func refreshBanners(_ invites: [UserInvite]? = nil) {
