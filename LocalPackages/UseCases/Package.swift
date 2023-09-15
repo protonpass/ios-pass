7eee86ff84624d5459bece5a0baa54dbc04dc988
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-09-15 15:40:05 +0200

    feat: add local Swift package for use cases

---
+++ LocalPackages/UseCases/Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+var platforms: [SupportedPlatform] = [
+    .macOS(.v12),
+    .iOS(.v15),
+    .tvOS(.v15),
+    .watchOS(.v8)
+]
+
+let package = Package(name: "UseCases",
+                      platforms: platforms,
+                      products: [
+                          // Products define the executables and libraries a package produces, and make them
+                          // visible to other packages.
+                          .library(name: "UseCases",
+                                   targets: ["UseCases"])
+                      ],
+                      dependencies: [
+                          // Dependencies declare other packages that this package depends on.
+                          .package(name: "Clien", path: "../Client"),
+                          .package(name: "Core", path: "../Core")
+                      ],
+                      targets: [
+                          // Targets are the basic building blocks of a package. A target can define a module or a
+                          // test suite.
+                          // Targets can depend on other targets in this package, and on products in packages this
+                          // package depends on.
+                          .target(name: "UseCases",
+                                  dependencies: [],
+                                  resources: []),
+                          .testTarget(name: "UseCasesTests",
+                                      dependencies: ["UseCases"])
+                      ])
