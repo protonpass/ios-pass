7c4d47453c44f995b2eeb1eaa19de651a93f7641
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-09-15 16:21:39 +0200

    feat(ci): add use cases unit tests to CI

--- LocalPackages/UseCases/Tests/UseCasesTests/TOTP/SanitizeTotpUriForEditingTests.swift
+++ LocalPackages/UseCases/Tests/UseCasesTests/TOTP/SanitizeTotpUriForEditingTests.swift
@@ -18,4 +18,11 @@
 // You should have received a copy of the GNU General Public License
 // along with Proton Pass. If not, see https://www.gnu.org/licenses/.
 
-import Foundation
+import UseCases
+import XCTest
+
+final class SanitizeTotpUriForEditingTests: XCTestCase {
+    func testFunction() {
+        XCTAssertTrue(true)
+    }
+}
