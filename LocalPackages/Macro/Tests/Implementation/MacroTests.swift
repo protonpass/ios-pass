fecda68bf06f98bb266c210c388038ce81c490a9
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-09-25 16:11:45 +0200

    feat: init Macro package

---
+++ LocalPackages/Macro/Tests/Implementation/MacroTests.swift
@@ -0,0 +1,43 @@
+import SwiftSyntaxMacros
+import SwiftSyntaxMacrosTestSupport
+import XCTest
+
+// Macro implementations build for the host, so the corresponding module is not available when cross-compiling.
+// Cross-compiled tests may still make use of the macro itself in end-to-end tests.
+#if canImport(MacroMacros)
+import MacroMacros
+
+let testMacros: [String: Macro.Type] = [
+    "stringify": StringifyMacro.self
+]
+#endif
+
+final class MacroTests: XCTestCase {
+    func testMacro() throws {
+        #if canImport(MacroMacros)
+        assertMacroExpansion("""
+                             #stringify(a + b)
+                             """,
+                             expandedSource: """
+                             (a + b, "a + b")
+                             """,
+                             macros: testMacros)
+        #else
+        throw XCTSkip("macros are only supported when running tests for the host platform")
+        #endif
+    }
+
+    func testMacroWithStringLiteral() throws {
+        #if canImport(MacroMacros)
+        assertMacroExpansion(#"""
+                             #stringify("Hello, \(name)")
+                             """#,
+                             expandedSource: #"""
+                             ("Hello, \(name)", #""Hello, \(name)""#)
+                             """#,
+                             macros: testMacros)
+        #else
+        throw XCTSkip("macros are only supported when running tests for the host platform")
+        #endif
+    }
+}
