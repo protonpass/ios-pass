1905bad0c49ab56e2d094c9dd72da9c6d28a7509
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-10-03 15:48:30 +0200

    feat: trigger share flow

--- iOS/Scenes/Homepage/Item Detail/Supporting Views/ItemDetailToolbar.swift
+++ iOS/Scenes/Homepage/Item Detail/Supporting Views/ItemDetailToolbar.swift
@@ -43,12 +43,17 @@ struct ItemDetailToolbar: ToolbarContent {
         ToolbarItem(placement: .navigationBarTrailing) {
             switch viewModel.itemContent.item.itemState {
             case .active:
-                HStack {
+                HStack(spacing: 0) {
                     CapsuleLabelButton(icon: IconProvider.pencil,
                                        title: #localized("Edit"),
                                        titleColor: PassColor.textInvert,
                                        backgroundColor: itemContentType.normMajor1Color,
                                        action: viewModel.edit)
+
+                    CircleButton(icon: IconProvider.usersPlus,
+                                 iconColor: itemContentType.normMajor2Color,
+                                 backgroundColor: itemContentType.normMinor1Color,
+                                 action: viewModel.share)
 
                     Menu(content: {
                         Button(action: viewModel.moveToAnotherVault,
