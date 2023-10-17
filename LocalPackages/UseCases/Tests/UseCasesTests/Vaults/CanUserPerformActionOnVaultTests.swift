691ef0cb7f32e9d9227a657960525af25892c54e
Martin Lukacs <martin.lukacs@proton.ch>
2023-10-17 14:11:46 +0200

    update for mr review and validation

---
+++ LocalPackages/UseCases/Tests/UseCasesTests/Vaults/CanUserPerformActionOnVaultTests.swift
@@ -0,0 +1,43 @@
+//
+// CanUserPerformActionOnVaultTests.swift
+// Proton Pass - Created on 17/10/2023.
+// Copyright (c) 2023 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+
+// import XCTest
+// @testable import UseCases
+//
+// final class CanUserPerformActionOnVaultTests: XCTestCase {
+//    var sut: CanUserPerformActionOnVault!
+//
+//    override func setUp() {
+//        super.setUp()
+//
+//        sut = CanUserPerformActionOnVault(planRepository: <#T##<<error type>>#>, vaultsManager: <#T##<<error
+//        type>>#>)
+//
+//    }
+//
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//        // Any test you write for XCTest can be annotated as throws and async.
+//        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+//        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with
+//        /assertions afterwards.
+//    }
+// }
