53ced6dcb28cce0ab677370c9134dc474efa319a
Martin Lukacs <martin.lukacs@proton.ch>
2023-11-07 17:22:12 +0100

    update of tests

--- LocalPackages/Client/Tests/ClientTests/Datasources/LocalFeatureFlagsDatasourceTests.swift
+++ LocalPackages/Client/Tests/ClientTests/Datasources/LocalFeatureFlagsDatasourceTests.swift
@@ -27,8 +27,7 @@ final class LocalFeatureFlagsDatasourceTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
-        sut = .init(container: .Builder.build(name: kProtonPassContainerName,
-                                              inMemory: true))
+        sut = .init(databaseService: DatabaseService(inMemory: true))
     }
 
     override func tearDown() {
