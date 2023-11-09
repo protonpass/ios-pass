0d83668ac9a0b265536313e7fd0827aa40ac103b
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-11-09 15:36:21 +0100

    feat: implement ApiServiceLite

---
+++ LocalPackages/Client/Sources/Client/Services/ApiServiceLite.swift
@@ -0,0 +1,58 @@
+//
+// ApiServiceLite.swift
+// Proton Pass - Created on 09/11/2023.
+// Copyright (c) 2023 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+//
+
+import Entities
+import Foundation
+import ProtonCoreEnvironment
+
+public protocol ApiServiceLiteProtocol {
+    typealias HTTPCode = Int
+
+    func execute(request: URLRequest) async throws -> HTTPCode
+}
+
+public final class ApiServiceLite: NSObject {
+    lazy var session: URLSession = .init(configuration: .ephemeral,
+                                         delegate: self,
+                                         delegateQueue: .main)
+}
+
+extension ApiServiceLite: ApiServiceLiteProtocol {
+    public func execute(request: URLRequest) async throws -> HTTPCode {
+        let (_, response) = try await session.data(for: request)
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw PassError.notHttpResponse
+        }
+        return httpResponse.statusCode
+    }
+}
+
+extension ApiServiceLite: URLSessionDelegate {
+    public func urlSession(_ session: URLSession,
+                           didReceive challenge: URLAuthenticationChallenge,
+                           completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?)
+                               -> Void) {
+        let validator = TrustKitWrapper.current?.pinningValidator
+        if validator?.handle(challenge, completionHandler: completionHandler) == false {
+            completionHandler(.performDefaultHandling, nil)
+        }
+    }
+}
