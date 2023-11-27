f15bc1451847a42f1ce3836b04256f98f820d447
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-11-27 18:11:26 +0100

    refactor: make generateItemContent non throwing function

--- iOS/Scenes/Homepage/Create & Edit Items/BaseCreateEditItemViewModel.swift
+++ iOS/Scenes/Homepage/Create & Edit Items/BaseCreateEditItemViewModel.swift
@@ -137,7 +137,7 @@ class BaseCreateEditItemViewModel {
 
     @MainActor
     // swiftlint:disable:next unavailable_function
-    func generateItemContent() throws -> ItemContentProtobuf? {
+    func generateItemContent() -> ItemContentProtobuf? {
         fatalError("Must be overridden by subclasses")
     }
 
@@ -194,7 +194,7 @@ private extension BaseCreateEditItemViewModel {
 
     func createItem(for type: ItemCreationType) async throws -> SymmetricallyEncryptedItem? {
         let shareId = selectedVault.shareId
-        guard let itemContent = try await generateItemContent() else {
+        guard let itemContent = await generateItemContent() else {
             logger.warning("No item content")
             return nil
         }
@@ -237,7 +237,7 @@ private extension BaseCreateEditItemViewModel {
                                                              itemId: itemId) else {
             throw PassError.itemNotFound(oldItemContent)
         }
-        guard let newItemContent = try await generateItemContent() else {
+        guard let newItemContent = await generateItemContent() else {
             logger.warning("No new item content")
             return
         }
