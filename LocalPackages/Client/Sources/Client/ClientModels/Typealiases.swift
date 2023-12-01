437b5ad32749c7833b7aec0cdef9734d052ed669
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2023-12-01 22:26:48 +0100

    feat: select vault to bulk moving

--- LocalPackages/Client/Sources/Client/ClientModels/Typealiases.swift
+++ LocalPackages/Client/Sources/Client/ClientModels/Typealiases.swift
@@ -26,3 +26,4 @@ public typealias UserDataSymmetricKeyProvider = SymmetricKeyProvider & UserDataP
 public typealias AppDataProtocol = CredentialProvider & Resettable & UserDataSymmetricKeyProvider
 public typealias VaultSyncEventStream = CurrentValueSubject<VaultSyncProgressEvent, Never>
 public typealias CorruptedSessionEventStream = PassthroughSubject<CorruptedSessionReason?, Never>
+public typealias ShareID = String
