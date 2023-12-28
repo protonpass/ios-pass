45c30b5e08d2291ddd04c91ca19c0defce99cdb8
Martin Lukacs <martin.lukacs@proton.ch>
2023-12-28 17:34:44 +0100

    fix: removed unused code

--- iOS/Scenes/Homepage/HomepageTabbarView.swift
+++ iOS/Scenes/Homepage/HomepageTabbarView.swift
@@ -107,10 +107,6 @@ final class HomepageTabBarController: UITabBarController, DeinitPrintable {
         fatalError("init(coder:) has not been implemented")
     }
 
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
         delegate = self
