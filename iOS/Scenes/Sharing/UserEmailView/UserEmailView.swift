2670981ea6c3c57a0b014c9a7213303ec6746b56
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2024-01-10 15:56:31 +0100

    refactor: improved email checks when continuing

--- iOS/Scenes/Sharing/UserEmailView/UserEmailView.swift
+++ iOS/Scenes/Sharing/UserEmailView/UserEmailView.swift
@@ -118,7 +118,7 @@ private extension UserEmailView {
                                               tintColor: PassColor.interactionNorm),
                                 onBackspace: { viewModel.highlightLastEmail() },
                                 onReturn: { viewModel.appendCurrentEmail() })
-            .frame(width: 150, height: 32)
+            .frame(width: max(150, CGFloat(viewModel.email.count) * 10), height: 32)
             .clipped()
     }
 
@@ -135,6 +135,7 @@ private extension UserEmailView {
 
         HStack(alignment: .center, spacing: 10) {
             Text(email)
+                .lineLimit(1)
         }
         .font(.callout)
         .foregroundColor(highlighted ? PassColor.textInvert.toColor : PassColor.textNorm.toColor)
