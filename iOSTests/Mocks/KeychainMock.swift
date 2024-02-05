9e49a19ca236fe7be6ddb21280d2028638d59543
Martin Lukacs <martin.lukacs@proton.ch>
2024-02-06 00:21:32 +0700

    fix: tests

--- iOSTests/Mocks/KeychainMock.swift
+++ iOSTests/Mocks/KeychainMock.swift
@@ -29,17 +29,17 @@ final class KeychainMock: KeychainProtocol {
 
     @FuncStub(KeychainMock.string, initialReturn: nil) var stringStub
 
-    func string(forKey key: String, attributes: [CFString: Any]?) -> String? { stringStub(key, attributes: ) }
+    func string(forKey key: String, attributes: [CFString: Any]?) -> String? { stringStub(key, attributes) }
 
-    @FuncStub(KeychainMock.set(data:forKey:)) var setDataStub
+    @FuncStub(KeychainMock.set(data:forKey:attributes:)) var setDataStub
 
-    private func set(data: Data, forKey key: String) { setDataStub(data, key, nil) }
-    func set(_ data: Data, forKey key: String, attributes: [CFString: Any]?) { set(data: data, forKey: key) }
+    private func set(data: Data, forKey key: String, attributes: [CFString: Any]? ) { setDataStub(data, key, attributes) }
+    func set(_ data: Data, forKey key: String, attributes: [CFString: Any]?) { set(data: data, forKey: key, attributes: attributes) }
 
-    @FuncStub(KeychainMock.set(string:forKey:)) var setStringStub
+    @FuncStub(KeychainMock.set(string:forKey:attributes:)) var setStringStub
 
-    private func set(string: String, forKey key: String) { setStringStub(string, key, nil) }
-    func set(_ string: String, forKey key: String, attributes: [CFString: Any]?) { set(string: string, forKey: key) }
+    private func set(string: String, forKey key: String, attributes: [CFString: Any]?) { setStringStub(string, key, attributes) }
+    func set(_ string: String, forKey key: String, attributes: [CFString: Any]?) { set(string: string, forKey: key, attributes: attributes) }
 
     @FuncStub(KeychainMock.remove) var removeStub
 
