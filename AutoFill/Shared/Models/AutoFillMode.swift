3f4b460493d7c75c177992a366d22ba4a3faa244
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2024-02-23 14:03:39 +0100

    feat(passkey): define autofill modes to take passkey into account

---
+++ AutoFill/Shared/Models/AutoFillMode.swift
@@ -0,0 +1,49 @@
+//
+// AutoFillMode.swift
+// Proton Pass - Created on 23/02/2024.
+// Copyright (c) 2024 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+
+import AuthenticationServices
+
+/// Possible entry points when autofilling
+enum AutoFillMode {
+    case showAllLogins(ShowAllLogins)
+    case checkAndAutoFill(CheckAndAutoFill)
+    case authenticateAndAutofill(AuthenticateAndAutofill)
+}
+
+/// User wants to manually select an item to autofill
+enum ShowAllLogins {
+    case password([URL])
+    case passkey([URL], PasskeyRequestParametersProtocol)
+}
+
+/// When user picks a proposed email from QuickType bar
+/// Check if user has local authentication enabled (Face ID/Touch ID/PIN)
+/// If authentication required: ask for authentication, pass to mode `AuthenticateAndAutofill`
+/// If authentication not required: autofill straight away
+enum CheckAndAutoFill {
+    case password(ASPasswordCredentialIdentity)
+    case passkey(PasskeyIdentityProtocol)
+}
+
+/// User picks a proposed email from QuickType bar but authentication (Face ID/Touch ID/PIN)  is required
+enum AuthenticateAndAutofill {
+    case password(ASPasswordCredentialIdentity)
+    case passkey(PasskeyIdentityProtocol)
+}
