114b37e415c6682f8716e8ca5e5a9a1871421e21
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2024-03-05 12:08:53 +0100

    refactor: make Entities independent from PassRustCore

--- LocalPackages/Entities/Package.swift
+++ LocalPackages/Entities/Package.swift
@@ -45,8 +45,7 @@ let package = Package(name: "Entities",
                       dependencies: [
                           // Dependencies declare other packages that this package depends on.
                           // .package(url: /* package url */, from: "1.0.0"),
-                        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.25.0"),
-                        .package(name: "PassRustCore", path: "../PassRustCore"),
+                        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.25.0")
                       ],
                       targets: [
                           // Targets are the basic building blocks of a package. A target can define a module or a
@@ -55,8 +54,7 @@ let package = Package(name: "Entities",
                           // package depends on.
                           .target(name: "Entities",
                                   dependencies: [
-                                    .product(name: "SwiftProtobuf", package: "swift-protobuf"),
-                                    .product(name: "PassRustCore", package: "PassRustCore"),
+                                    .product(name: "SwiftProtobuf", package: "swift-protobuf")
                                   ],
                                   swiftSettings: swiftSettings),
                           .target(
