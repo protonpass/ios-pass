0ddf74fd3cba504a830775dfd7b81542d22a89a3
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2024-03-22 13:40:07 +0100

    feat: show item force touch tip

--- iOS/Scenes/Homepage/Items Tab/ItemsTabViewModel.swift
+++ iOS/Scenes/Homepage/Items Tab/ItemsTabViewModel.swift
@@ -186,6 +186,10 @@ private extension ItemsTabViewModel {
                 await banners.append(contentsOf: localBanners())
             }
             self.banners = banners
+
+            if #available(iOS 17, *), banners.isEmpty {
+                ItemForceTouchTip.allBannersDismissed = true
+            }
         }
     }
 
