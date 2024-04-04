a4552e9e08c16a32aa84b8ab51e4d0c31e71e0f1
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2024-04-04 14:50:45 +0200

    feat: make all preferences decodable with default values

--- LocalPackages/Entities/Sources/Entities/Models/Configuration/AppPreferences.swift
+++ LocalPackages/Entities/Sources/Entities/Models/Configuration/AppPreferences.swift
@@ -35,15 +35,53 @@ public struct AppPreferences: Codable, Equatable, Sendable {
     /// Keep track of dismissed banners so we don't show them again
     public var dismissedBannerIds: [String]
 
-    public static var `default`: Self { .init() }
-
-    public init(onboarded: Bool = false,
-                telemetryThreshold: TimeInterval? = nil,
-                createdItemsCount: Int = 0,
-                dismissedBannerIds: [String] = []) {
+    public init(onboarded: Bool,
+                telemetryThreshold: TimeInterval?,
+                createdItemsCount: Int,
+                dismissedBannerIds: [String]) {
         self.onboarded = onboarded
         self.telemetryThreshold = telemetryThreshold
         self.createdItemsCount = createdItemsCount
         self.dismissedBannerIds = dismissedBannerIds
+    }
+}
+
+private extension AppPreferences {
+    enum Default {
+        static let onboarded = false
+        static let telemetryThreshold: TimeInterval? = nil
+        static let createdItemsCount = 0
+        static let dismissedBannerIds: [String] = []
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case onboarded
+        case telemetryThreshold
+        case createdItemsCount
+        case dismissedBannerIds
+    }
+}
+
+public extension AppPreferences {
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let onboarded = try container.decodeIfPresent(Bool.self, forKey: .onboarded)
+        let telemetryThreshold = try container.decodeIfPresent(TimeInterval.self,
+                                                               forKey: .telemetryThreshold)
+        let createdItemsCount = try container.decodeIfPresent(Int.self, forKey: .createdItemsCount)
+        let dismissedBannerIds = try container.decodeIfPresent([String].self, forKey: .dismissedBannerIds)
+        self.init(onboarded: onboarded ?? Default.onboarded,
+                  telemetryThreshold: telemetryThreshold ?? Default.telemetryThreshold,
+                  createdItemsCount: createdItemsCount ?? Default.createdItemsCount,
+                  dismissedBannerIds: dismissedBannerIds ?? Default.dismissedBannerIds)
+    }
+}
+
+public extension AppPreferences {
+    static var `default`: Self {
+        .init(onboarded: Default.onboarded,
+              telemetryThreshold: Default.telemetryThreshold,
+              createdItemsCount: Default.createdItemsCount,
+              dismissedBannerIds: Default.dismissedBannerIds)
     }
 }
---
+++ LocalPackages/Entities/Tests/EntitiesTests/PreferencesTests.swift
@@ -0,0 +1,97 @@
+//
+// PreferencesTests.swift
+// Proton Pass - Created on 04/04/2024.
+// Copyright (c) 2024 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+//
+
+import Entities
+import XCTest
+
+final class PreferencesTests: XCTestCase {
+    func testDecodeAppPreferencesFromEmptyJson() throws {
+        try decodeAndAssert(AppPreferences.self, json: "{}", expectation: .default)
+    }
+
+    func testDecodeAppPrerencesFromLegacyJson() throws {
+        let json = """
+{
+    "createdItemsCount": 100,
+    "dismissedBannerIds": ["a", "b", "c"]
+}
+"""
+        let expectation = AppPreferences(onboarded: AppPreferences.default.onboarded,
+                                         telemetryThreshold: AppPreferences.default.telemetryThreshold,
+                                         createdItemsCount: 100,
+                                         dismissedBannerIds: ["a", "b", "c"])
+        try decodeAndAssert(AppPreferences.self, json: json, expectation: expectation)
+    }
+
+    func testDecodeSharedPreferencesFromEmptyJson() throws {
+        try decodeAndAssert(SharedPreferences.self, json: "{}", expectation: .default)
+    }
+
+    func testDecodeSharedPrerencesFromLegacyJson() throws {
+        let json = """
+{
+    "shareClipboard": true,
+    "localAuthenticationMethod": {
+        "pin": {}
+    },
+    "clipboardExpiration": 2
+}
+"""
+        let defaultObject = SharedPreferences.default
+        let expectation = SharedPreferences(
+            quickTypeBar: defaultObject.quickTypeBar,
+            automaticallyCopyTotpCode: defaultObject.automaticallyCopyTotpCode,
+            theme: defaultObject.theme,
+            browser: defaultObject.browser,
+            displayFavIcons: defaultObject.displayFavIcons,
+            failedAttemptCount: defaultObject.failedAttemptCount,
+            localAuthenticationMethod: .pin,
+            pinCode: defaultObject.pinCode,
+            fallbackToPasscode: SharedPreferences.default.fallbackToPasscode,
+            appLockTime: defaultObject.appLockTime,
+            clipboardExpiration: .twoMinutes,
+            shareClipboard: true)
+        try decodeAndAssert(SharedPreferences.self, json: json, expectation: expectation)
+    }
+
+    func testDecodeUserPreferencesFromEmptyJson() throws {
+        try decodeAndAssert(UserPreferences.self, json: "{}", expectation: .default)
+    }
+
+    func testDecodeUserPrerencesFromLegacyJson() throws {
+        let json = """
+{
+    "spotlightSearchableContent": 2
+}
+"""
+        let expectation = UserPreferences(
+            spotlightEnabled: UserPreferences.default.spotlightEnabled,
+            spotlightSearchableContent: .allExceptSensitiveData,
+            spotlightSearchableVaults: UserPreferences.default.spotlightSearchableVaults)
+        try decodeAndAssert(UserPreferences.self, json: json, expectation: expectation)
+    }
+
+    func decodeAndAssert<T: Decodable & Equatable>(_ type: T.Type, json: String, expectation: T) throws {
+        let data = try XCTUnwrap(json.data(using: .utf8))
+        let result = try JSONDecoder().decode(T.self, from: data)
+        XCTAssertEqual(result, expectation)
+    }
+}
