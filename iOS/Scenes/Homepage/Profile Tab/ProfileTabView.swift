67831e3dbb326d57fc5c6dedaff4b6649588e249
Martin Lukacs <martin.lukacs@proton.ch>
2024-04-09 13:17:54 +0300

    feat: added use case and remove code form profil

--- iOS/Scenes/Homepage/Profile Tab/ProfileTabView.swift
+++ iOS/Scenes/Homepage/Profile Tab/ProfileTabView.swift
@@ -27,21 +27,11 @@ import SwiftUI
 
 struct ProfileTabView: View {
     @StateObject var viewModel: ProfileTabViewModel
-    @State private var presentSentinelSheet = false
 
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack {
-                    if viewModel.sentinelEnabled, viewModel.isSentinelEligible {
-                        Button { presentSentinelSheet = true } label: {
-                            sentinelCell
-                                .padding(.horizontal)
-                                .padding(.bottom)
-                                .showSpinner(viewModel.updatingSentinel)
-                        }
-                        .buttonStyle(.plain)
-                    }
                     itemCountSection
 
                     securitySection
@@ -73,7 +63,6 @@ struct ProfileTabView: View {
                 .padding(.top)
                 .animation(.default, value: viewModel.automaticallyCopyTotpCode)
                 .animation(.default, value: viewModel.localAuthenticationMethod)
-                .animation(.default, value: viewModel.isSentinelEligible)
                 .showSpinner(viewModel.loading)
             }
 
@@ -85,16 +74,6 @@ struct ProfileTabView: View {
         }
         .task {
             await viewModel.refreshPlan()
-            await viewModel.checkSentinel()
-        }
-        .sheet(isPresented: $presentSentinelSheet) {
-            SentinelSheetView(isPresented: $presentSentinelSheet,
-                              sentinelActive: viewModel.isSentinelActive,
-                              mainAction: { viewModel.toggleSentinelState()
-                                  presentSentinelSheet = false
-                              },
-                              secondaryAction: { viewModel.showSentinelInformation() })
-                .presentationDetents([.height(500)])
         }
         .navigationViewStyle(.stack)
     }
@@ -354,59 +333,6 @@ struct ProfileTabView: View {
     }
 }
 
-// MARK: - Sentinel
-
-private extension ProfileTabView {
-    var sentinelCell: some View {
-        HStack(spacing: DesignConstant.sectionPadding) {
-            ZStack(alignment: .bottomTrailing) {
-                Image(uiImage: PassIcon.sentinelLogo)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 40)
-
-                Image(systemName: viewModel.isSentinelActive ? "checkmark.circle.fill" : "xmark.circle.fill")
-                    .resizable()
-                    .frame(width: 12, height: 12)
-                    .foregroundColor(viewModel.isSentinelActive ? PassColor.interactionNormMajor2.toColor :
-                        PassColor.noteInteractionNormMajor2.toColor)
-                    .background(viewModel.isSentinelActive ? .white : .black)
-                    .clipShape(.circle)
-            }
-
-            VStack(alignment: .leading, spacing: DesignConstant.sectionPadding / 4) {
-                Text(verbatim: "Proton Sentinel ")
-                    .font(.body)
-                    .foregroundColor(PassColor.textNorm.toColor)
-                    + Text(verbatim: viewModel.isSentinelActive ? #localized("Active") : #localized("Inactive"))
-                    .font(.body)
-                    .foregroundColor(viewModel.isSentinelActive ? PassColor.interactionNormMajor2
-                        .toColor : PassColor.noteInteractionNormMajor2.toColor)
-                Text("Increase your security")
-                    .font(.footnote)
-                    .foregroundColor(PassColor.textWeak.toColor)
-            }
-            .frame(maxWidth: .infinity, minHeight: 75, alignment: .leading)
-            .contentShape(Rectangle())
-            ItemDetailSectionIcon(icon: IconProvider.chevronRight,
-                                  color: PassColor.textWeak,
-                                  width: 15)
-        }
-        .padding(.horizontal, DesignConstant.sectionPadding)
-        .background(PassColor.inputBackgroundNorm.toColor)
-        .clipShape(RoundedRectangle(cornerRadius: 16))
-        .overlay(RoundedRectangle(cornerRadius: 16)
-            .stroke(LinearGradient(gradient:
-                Gradient(colors: [
-                    PassColor.interactionNormMajor2.toColor,
-                    PassColor.noteInteractionNormMajor2.toColor
-                ]),
-                startPoint: .leading,
-                endPoint: .trailing),
-            lineWidth: 1))
-    }
-}
-
 private extension View {
     func profileSectionTitle() -> some View {
         foregroundColor(PassColor.textNorm.toColor)
@@ -451,10 +377,18 @@ struct SentinelSheetView: View {
 
     var body: some View {
         ZStack(alignment: .topTrailing) {
-            PassColor.backgroundNorm.toColor
+            Color.white
                 .ignoresSafeArea()
 
-            ViewThatFits {
+            LinearGradient(colors: [
+                .clear,
+                Color(red: 112 / 255, green: 76 / 255, blue: 225 / 255, opacity: 0.15)
+            ],
+            startPoint: .top,
+            endPoint: .bottom)
+                .ignoresSafeArea()
+
+            ViewThatFits(in: .vertical) {
                 mainSentinelSheet.padding(20)
                 ScrollView(showsIndicators: false) {
                     mainSentinelSheet
@@ -465,13 +399,14 @@ struct SentinelSheetView: View {
                 Image(systemName: "xmark.circle.fill")
                     .resizable()
                     .frame(width: 30, height: 30)
-                    .foregroundColor(PassColor.interactionNormMinor1.toColor)
-                    .background(PassColor.interactionNormMajor2.toColor)
+                    .foregroundColor(.black)
+                    .background(PassColor.interactionNormMinor1.toColor)
                     .clipShape(.circle)
             }
             .buttonStyle(.plain)
             .padding(15)
         }
+        .preferredColorScheme(.light)
     }
 
     private var mainSentinelSheet: some View {
@@ -483,18 +418,19 @@ struct SentinelSheetView: View {
             Text("Proton Sentinel")
                 .font(.title)
                 .fontWeight(.bold)
-                .foregroundColor(.white)
+                .foregroundStyle(PassColor.textInvert.toColor)
 
             Text("Sentinel description")
                 .font(.body)
                 .multilineTextAlignment(.center)
-                .foregroundColor(.white)
+                .foregroundStyle(PassColor.textInvert.toColor)
                 .frame(maxWidth: .infinity, alignment: .top)
 
             CapsuleTextButton(title: sentinelActive ? #localized("Disable Proton Sentinel") :
                 #localized("Enable Proton Sentinel"),
-                titleColor: PassColor.interactionNormMinor2,
-                backgroundColor: PassColor.interactionNormMajor1,
+                titleColor: PassColor.interactionNormMinor1,
+                backgroundColor: PassColor.interactionNormMajor2,
+                height: 48,
                 action: {
                     mainAction()
                 })
@@ -503,6 +439,7 @@ struct SentinelSheetView: View {
             CapsuleTextButton(title: #localized("Learn more"),
                               titleColor: PassColor.interactionNormMajor2,
                               backgroundColor: PassColor.interactionNormMinor1,
+                              height: 48,
                               action: { secondaryAction() })
                 .padding(.horizontal, DesignConstant.sectionPadding)
         }
