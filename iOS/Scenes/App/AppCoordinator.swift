737ea8b4b986308585fcca879815e44475424041
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2024-04-10 18:41:14 +0200

    feat: remove shared preferences when app is reinstalled

--- iOS/Scenes/App/AppCoordinator.swift
+++ iOS/Scenes/App/AppCoordinator.swift
@@ -158,7 +158,7 @@ final class AppCoordinator {
                     appStateObserver.updateAppState(.loggedOut(.noSessionDataAtAll))
                 }
             } catch {
-                alert(title: #localized("Error occured"), message: error.localizedDescription)
+                appStateObserver.updateAppState(.loggedOut(.failedToInitializePreferences(error)))
             }
         }
     }
@@ -276,6 +276,8 @@ private extension AppCoordinator {
         case .failedBiometricAuthentication:
             alert(title: #localized("Failed to authenticate"),
                   message: #localized("Please log in again"))
+        case let .failedToInitializePreferences(error):
+            alert(title: #localized("Error occured"), message: error.localizedDescription)
         default:
             break
         }
