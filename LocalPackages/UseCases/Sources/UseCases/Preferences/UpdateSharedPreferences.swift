c4b1a937ff96cd3bd4dcb6fb4e264baec0e2d408
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2024-04-12 13:56:04 +0200

    feat: add use cases to retrieve and get preferences

---
+++ LocalPackages/UseCases/Sources/UseCases/Preferences/UpdateSharedPreferences.swift
@@ -0,0 +1,49 @@
+//
+// UpdateSharedPreferences.swift
+// Proton Pass - Created on 12/04/2024.
+// Copyright (c) 2024 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+//
+
+import Client
+import Entities
+import Foundation
+
+public protocol UpdateSharedPreferencesUseCase: Sendable {
+    func execute<T: Sendable>(_ keyPath: WritableKeyPath<SharedPreferences, T>,
+                              value: T) async throws
+}
+
+public extension UpdateSharedPreferencesUseCase {
+    func callAsFunction<T: Sendable>(_ keyPath: WritableKeyPath<SharedPreferences, T>,
+                                     value: T) async throws {
+        try await execute(keyPath, value: value)
+    }
+}
+
+public final class UpdateSharedPreferences: UpdateSharedPreferencesUseCase {
+    private let manager: any PreferencesManagerProtocol
+
+    public init(manager: any PreferencesManagerProtocol) {
+        self.manager = manager
+    }
+
+    public func execute<T: Sendable>(_ keyPath: WritableKeyPath<SharedPreferences, T>,
+                                     value: T) async throws {
+        try await manager.updateSharedPreferences(keyPath, value: value)
+    }
+}
