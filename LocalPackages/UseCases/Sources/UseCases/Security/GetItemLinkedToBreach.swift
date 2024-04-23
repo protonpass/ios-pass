01a190cbb1641c4cddc95b2e0f6bc1a8c82ff718
Martin Lukacs <martin.lukacs@proton.ch>
2024-04-23 18:22:47 +0200

    feat: added breach detail view

---
+++ LocalPackages/UseCases/Sources/UseCases/Security/GetItemLinkedToBreach.swift
@@ -0,0 +1,58 @@
+//
+//
+// GetItemLinkedToBreach.swift
+// Proton Pass - Created on 23/04/2024.
+// Copyright (c) 2024 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+//
+
+import Client
+import Entities
+
+public protocol GetItemLinkedToBreachUseCase: Sendable {
+    func execute(email: String) async throws -> [ItemUiModel]
+}
+
+public extension GetItemLinkedToBreachUseCase {
+    func callAsFunction(email: String) async throws -> [ItemUiModel] {
+        try await execute(email: email)
+    }
+}
+
+public final class GetItemLinkedToBreach: GetItemLinkedToBreachUseCase {
+    private let symmetricKeyProvider: any SymmetricKeyProvider
+    private let repository: any ItemRepositoryProtocol
+
+    public init(symmetricKeyProvider: any SymmetricKeyProvider,
+                repository: any ItemRepositoryProtocol) {
+        self.symmetricKeyProvider = symmetricKeyProvider
+        self.repository = repository
+    }
+
+    public func execute(email: String) async throws -> [ItemUiModel] {
+        let symmetricKey = try symmetricKeyProvider.getSymmetricKey()
+        let encryptedItems = try await repository.getAllItems()
+        return encryptedItems.compactMap { element in
+            guard let uimodel = try? element.toItemUiModel(symmetricKey),
+                  uimodel.type == .login || uimodel.type == .alias,
+                  uimodel.description.contains(email) else {
+                return nil
+            }
+            return uimodel
+        }
+    }
+}
