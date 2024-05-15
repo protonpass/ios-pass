f13d5a382df2e9bcb82ed46e6db151906aac1f9e
Martin Lukacs <martin.lukacs@proton.ch>
2024-05-15 14:57:54 +0200

    feat: added public link endpoint

---
+++ LocalPackages/Client/Sources/Client/Networking/Endpoints/Pass/PublicLink/GetAllPublicLinksForUserEndpoint.swift
@@ -0,0 +1,47 @@
+//
+// GetAllPublicLinksForUserEndpoint.swift
+// Proton Pass - Created on 15/05/2024.
+// Copyright (c) 2024 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+
+import Entities
+import ProtonCoreNetworking
+import ProtonCoreServices
+
+struct GetAllPublicLinksForUserResponse: Decodable, Equatable, Sendable {
+    let code: Int
+    let publicLinks: PublicLinks
+}
+
+struct PublicLinks: Decodable, Equatable, Sendable {
+    let publicLinks: [PublicLink]
+}
+
+struct GetAllPublicLinksForUserEndpoint: Endpoint {
+    typealias Body = EmptyRequest
+    typealias Response = GetAllPublicLinksForUserResponse
+
+    var debugDescription: String
+    var path: String
+    var method: HTTPMethod
+
+    init(publicLinkId: String) {
+        debugDescription = "Create new public link"
+        path = "/pass/v1/public_link/\(publicLinkId)"
+        method = .get
+    }
+}
