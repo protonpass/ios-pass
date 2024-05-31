b758e9226514913f3974a0208bb05311875c495d
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2024-05-31 12:40:56 +0200

    feat: verify Proton password

--- LocalPackages/UseCases/Sources/UseCases/User/VerifyProtonPassword.swift
+++ LocalPackages/UseCases/Sources/UseCases/User/VerifyProtonPassword.swift
@@ -19,7 +19,16 @@
 // along with Proton Pass. If not, see https://www.gnu.org/licenses/.
 //
 
+import Client
+import Entities
 import Foundation
+import ProtonCoreAuthentication
+import ProtonCoreChallenge
+import ProtonCoreCryptoGoInterface
+import ProtonCoreDoh
+import ProtonCoreFoundations
+import ProtonCoreNetworking
+import ProtonCoreServices
 
 public protocol VerifyProtonPasswordUseCase: Sendable {
     func execute(_ password: String) async throws -> Bool
@@ -32,10 +41,107 @@ public extension VerifyProtonPasswordUseCase {
 }
 
 public final class VerifyProtonPassword: Sendable, VerifyProtonPasswordUseCase {
-    public init() {}
+    private let userDataProvider: any UserDataProvider
+    private let doh: any DoHInterface
+    private let appVer: String
+
+    // Required by `AuthDelegate`
+    // swiftlint:disable:next identifier_name
+    public var authSessionInvalidatedDelegateForLoginAndSignup: (any AuthSessionInvalidatedDelegate)?
+
+    private var apiService: (any APIService)?
+
+    public init(userDataProvider: any UserDataProvider,
+                doh: any DoHInterface,
+                appVer: String) {
+        self.userDataProvider = userDataProvider
+        self.doh = doh
+        self.appVer = appVer
+    }
 
     public func execute(_ password: String) async throws -> Bool {
-        try await Task.sleep(seconds: 3)
-        return .random()
+        guard let userData = userDataProvider.getUserData() else {
+            throw PassError.noUserData
+        }
+
+        let authenticator = Authenticator(api: makeApiService())
+        return try await withCheckedThrowingContinuation { continuation in
+            authenticator.authenticate(username: userData.credential.userName,
+                                       password: password,
+                                       challenge: nil) { result in
+                switch result {
+                case .success:
+                    continuation.resume(returning: true)
+                case let .failure(error):
+                    switch error {
+                    /// Upon testing`wrongPassword` case is not returned by this method but just in case
+                    case .wrongPassword:
+                        continuation.resume(returning: false)
+                    case let .networkingError(networkError):
+                        if networkError.httpCode == 422,
+                           networkError.responseCode == 8_002 {
+                            continuation.resume(returning: false)
+                        } else {
+                            continuation.resume(throwing: networkError)
+                        }
+                    default:
+                        continuation.resume(throwing: error)
+                    }
+                }
+            }
+        }
     }
+}
+
+private extension VerifyProtonPassword {
+    func makeApiService() -> any APIService {
+        if let apiService {
+            return apiService
+        }
+        // Create an unauth api service on the fly otherwise wrong verifications
+        // would expire the current session (log the user out)
+        let challengeProvider = ChallengeParametersProvider.forAPIService(clientApp: .pass,
+                                                                          challenge: .init())
+        let apiService = PMAPIService.createAPIServiceWithoutSession(doh: doh,
+                                                                     challengeParametersProvider: challengeProvider)
+        apiService.serviceDelegate = self
+        apiService.authDelegate = self
+        self.apiService = apiService
+        return apiService
+    }
+}
+
+// MARK: APIServiceDelegate
+
+extension VerifyProtonPassword: APIServiceDelegate {
+    public var appVersion: String { appVer }
+    public var userAgent: String? { UserAgent.default.ua }
+    public var locale: String { Locale.autoupdatingCurrent.identifier }
+    public var additionalHeaders: [String: String]? { nil }
+
+    public func onDohTroubleshot() {}
+
+    public func onUpdate(serverTime: Int64) {
+        CryptoGo.CryptoUpdateTime(serverTime)
+    }
+
+    public func isReachable() -> Bool {
+        true
+    }
+}
+
+// MARK: AuthDelegate
+
+/// Do nothing, just to make the `APIService` happy because it expectes an `AuthDelegate`
+extension VerifyProtonPassword: AuthDelegate {
+    public func authCredential(sessionUID: String) -> AuthCredential? { nil }
+    public func credential(sessionUID: String) -> Credential? { nil }
+    public func onUpdate(credential: Credential, sessionUID: String) {}
+    public func onSessionObtaining(credential: Credential) {}
+    public func onAdditionalCredentialsInfoObtained(sessionUID: String,
+                                                    password: String?,
+                                                    salt: String?,
+                                                    privateKey: String?) {}
+    public func onAuthenticatedSessionInvalidated(sessionUID: String) {}
+    public func onUnauthenticatedSessionInvalidated(sessionUID: String) {}
 }
