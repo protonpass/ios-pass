03c9cc026ebccd0b574b81ede5dc197c5768c7f5
Martin Lukacs <martin.lukacs@proton.ch>
2024-06-05 17:28:11 +0200

    fix:  Little clean of code
    - removed some old commented code
    - remove strict concurrency warnings

--- iOS/Shared/Domain/UseCases/WipeAllData.swift
+++ iOS/Shared/Domain/UseCases/WipeAllData.swift
@@ -24,7 +24,7 @@ import Foundation
 import ProtonCoreFeatureFlags
 import UIKit
 
-protocol WipeAllDataUseCase {
+protocol WipeAllDataUseCase: Sendable {
     func execute() async
 }
 
@@ -33,6 +33,8 @@ extension WipeAllDataUseCase {
         await execute()
     }
 }
+
+extension VaultSyncEventStream: @unchecked Sendable{}
 
 final class WipeAllData: WipeAllDataUseCase {
     private let logger: Logger
