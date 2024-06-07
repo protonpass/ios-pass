11a327eff03a4d63cc9a17cbc58facd9654b022e
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2024-06-07 11:41:32 +0200

    feat: functions to init extra password validation and validate extra password

---
+++ LocalPackages/Client/Sources/Client/Networking/Endpoints/Pass/User/InitiateSrpAuthenticationEndpoint.swift
@@ -0,0 +1,40 @@
+//
+// InitiateSrpAuthenticationEndpoint.swift
+// Proton Pass - Created on 07/06/2024.
+// Copyright (c) 2024 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+//
+
+import Entities
+import ProtonCoreNetworking
+
+struct InitiateSrpAuthenticationResponse: Decodable, Sendable {
+    let srpData: SrpAuthenticationData
+
+    enum CodingKeys: String, CodingKey {
+        case srpData = "SRPData"
+    }
+}
+
+struct InitiateSrpAuthenticationEndpoint: Endpoint {
+    typealias Body = EmptyRequest
+    typealias Response = InitiateSrpAuthenticationResponse
+
+    let debugDescription = "Initiate SRP authentication"
+    let path = "/pass/v1/user/srp/info"
+    let method: HTTPMethod = .get
+}
