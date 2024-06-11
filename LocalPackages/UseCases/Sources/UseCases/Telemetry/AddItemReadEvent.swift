a21490104ec8647035791fbea002c9c074814e4f
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2024-06-11 15:18:53 +0200

    feat: force send all events for testing

---
+++ LocalPackages/UseCases/Sources/UseCases/Telemetry/AddItemReadEvent.swift
@@ -0,0 +1,62 @@
+//
+// AddItemReadEvent.swift
+// Proton Pass - Created on 11/06/2024.
+// Copyright (c) 2024 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+//
+
+import Client
+import Core
+import Entities
+import Foundation
+
+public protocol AddItemReadEventUseCase: Sendable {
+    func execute(_ item: any ItemIdentifiable)
+}
+
+public extension AddItemReadEventUseCase {
+    func callAsFunction(_ item: any ItemIdentifiable) {
+        execute(item)
+    }
+}
+
+public final class AddItemReadEvent: Sendable, AddItemReadEventUseCase {
+    private let eventRepository: any ItemReadEventRepositoryProtocol
+    private let accessRepository: any AccessRepositoryProtocol
+    private let logger: Logger
+
+    public init(eventRepository: any ItemReadEventRepositoryProtocol,
+                accessRepository: any AccessRepositoryProtocol,
+                logManager: any LogManagerProtocol) {
+        self.eventRepository = eventRepository
+        self.accessRepository = accessRepository
+        logger = .init(manager: logManager)
+    }
+
+    public func execute(_ item: any ItemIdentifiable) {
+        Task { [weak self] in
+            guard let self else { return }
+            do {
+                if accessRepository.access.value?.plan.isBusinessUser == true {
+                    try await eventRepository.addEvent(for: item)
+                }
+            } catch {
+                logger.error(error)
+            }
+        }
+    }
+}
