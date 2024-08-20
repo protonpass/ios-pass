e0d9781ad81e32529a200b1d19fcc24b29d26131
Martin Lukacs <martin.lukacs@proton.ch>
2024-08-20 17:25:14 +0200

    feat: implementation of new upsert

--- LocalPackages/Client/Sources/Client/Datasources/Local/LocalAccessDatasource.swift
+++ LocalPackages/Client/Sources/Client/Datasources/Local/LocalAccessDatasource.swift
@@ -49,15 +49,30 @@ public extension LocalAccessDatasource {
         return entities.compactMap { $0.toUserAccess() }
     }
 
-    func upsert(access: UserAccess) async throws {
-        let taskContext = newTaskContext(type: .insert)
+//    func upsert(access: UserAccess) async throws {
+//        let taskContext = newTaskContext(type: .insert)
+//
+//        let batchInsertRequest =
+//            newBatchInsertRequest(entity: AccessEntity.entity(context: taskContext),
+//                                  sourceItems: [access]) { managedObject, access in
+//                (managedObject as? AccessEntity)?.hydrate(from: access)
+//            }
+//        try await execute(batchInsertRequest: batchInsertRequest, context: taskContext)
+//    }
 
-        let batchInsertRequest =
-            newBatchInsertRequest(entity: AccessEntity.entity(context: taskContext),
-                                  sourceItems: [access]) { managedObject, access in
-                (managedObject as? AccessEntity)?.hydrate(from: access)
-            }
-        try await execute(batchInsertRequest: batchInsertRequest, context: taskContext)
+    func upsert(access: UserAccess) async throws {
+        try await upsertElements(items: [access],
+                                 fetchPredicate: NSPredicate(format: "userID == %@", access.userId),
+                                 itemComparisonKey: { item in
+                                     AccessKeyComparison(userId: item.userId)
+                                 },
+                                 entityComparisonKey: { entity in
+                                     AccessKeyComparison(userId: entity.userID)
+                                 },
+                                 updateEntity: { (entity: AccessEntity, item: UserAccess) in
+                                     entity.hydrate(from: item)
+                                 },
+                                 insertItems: insertAccess)
     }
 
     func removeAccess(userId: String) async throws {
@@ -67,4 +82,25 @@ public extension LocalAccessDatasource {
         try await execute(batchDeleteRequest: .init(fetchRequest: fetchRequest),
                           context: deleteContext)
     }
+}
+
+private extension LocalAccessDatasource {
+    struct AccessKeyComparison: Hashable {
+        let userId: String
+    }
+
+    func insertAccess(access: [UserAccess]) async throws {
+        let taskContext = newTaskContext(type: .insert)
+
+        let batchInsertRequest =
+            newBatchInsertRequest(entity: AccessEntity.entity(context: taskContext),
+                                  sourceItems: access) { managedObject, access in
+                (managedObject as? AccessEntity)?.hydrate(from: access)
+            }
+        try await execute(batchInsertRequest: batchInsertRequest, context: taskContext)
+    }
+
+//    func updateEntity(_ entity: AccessEntity, with newItem: UserAccess) {
+//        entity.hydrate(from: newItem)
+//    }
 }
