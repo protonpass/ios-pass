c97cc84df3abc59639f19d639bc88007e5542a7f
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2024-09-11 21:47:40 +0200

    fix: make Xcode build to real devices again
    Remove the script that update the git commit hash from Build Phases settings because it has no effects as a build phase

--- scripts/public/update_git_commit_hash.sh
+++ scripts/public/update_git_commit_hash.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Workaround a Xcode bug that fails to build to real devices if we alter the Info.plist
+# So we only update the git commit hash when building to simulators or making releases
+if [[ "$EFFECTIVE_PLATFORM_NAME" == "-iphonesimulator" || "$CONFIGURATION" == "Release"* ]]; then
+    echo "Building to simulator or making a release. Updating git commit hash."
+else
+    echo "Building to real devices. Skip updating git commit hash."
+    exit 0
+fi
+
 INFOPLIST_PATH="${TARGET_BUILD_DIR}/${EXECUTABLE_NAME}.app/Info.plist"
 
 # Exit 
