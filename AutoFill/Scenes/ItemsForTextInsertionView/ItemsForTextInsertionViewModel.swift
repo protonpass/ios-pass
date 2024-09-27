b7e2c6d88b8a09709daa23696d98ffccff1b5423
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2024-09-27 16:26:08 +0200

    feat: fetch and show all items of all types

--- AutoFill/Scenes/ItemsForTextInsertionView/ItemsForTextInsertionViewModel.swift
+++ AutoFill/Scenes/ItemsForTextInsertionView/ItemsForTextInsertionViewModel.swift
@@ -18,11 +18,85 @@
 // You should have received a copy of the GNU General Public License
 // along with Proton Pass. If not, see https://www.gnu.org/licenses/.
 
+import Client
+import Entities
+import Factory
 import SwiftUI
+
+enum ItemsForTextInsertionViewState: Equatable {
+    /// Empty search query
+    case idle
+    case searching
+    case searchResults([ItemSearchResult])
+    case loading
+    case error(any Error)
+
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        switch (lhs, rhs) {
+        case let (.error(lhsError), .error(rhsError)):
+            lhsError.localizedDescription == rhsError.localizedDescription
+        case (.idle, .idle),
+             (.loading, .loading),
+             (.searching, .searching),
+             (.searchResults, .searchResults):
+            true
+        default:
+            false
+        }
+    }
+}
 
 @MainActor
 final class ItemsForTextInsertionViewModel: AutoFillViewModel<ItemsForTextInsertion> {
+    @Published private(set) var state = CredentialsViewState.loading
     @Published var query = ""
+
+    @LazyInjected(\AutoFillUseCaseContainer.fetchItemsForTextInsertion)
+    private var fetchItemsForTextInsertion
+
+    private var searchableItems: [SearchableItem] {
+        if let selectedUser {
+            results.first { $0.userId == selectedUser.id }?.searchableItems ?? []
+        } else {
+            getAllObjects(\.searchableItems)
+        }
+    }
+
+    var items: [ItemUiModel] {
+        if let selectedUser {
+            results.first { $0.userId == selectedUser.id }?.items ?? []
+        } else {
+            getAllObjects(\.items)
+        }
+    }
+
+    override func getVaults(userId: String) -> [Vault]? {
+        results.first { $0.userId == userId }?.vaults
+    }
+
+    override func isErrorState() -> Bool {
+        if case .error = state {
+            true
+        } else {
+            false
+        }
+    }
+
+    override func fetchAutoFillCredentials(userId: String) async throws -> ItemsForTextInsertion {
+        try await fetchItemsForTextInsertion(userId: userId)
+    }
+
+    override func changeToErrorState(_ error: any Error) {
+        state = .error(error)
+    }
+
+    override func changeToLoadingState() {
+        state = .loading
+    }
+
+    override func changeToLoadedState() {
+        state = .idle
+    }
 }
 
 extension ItemsForTextInsertionViewModel {
---
+++ AutoFill/Shared/UseCases/FetchItemsForTextInsertion.swift
@@ -0,0 +1,87 @@
+//
+// FetchItemsForTextInsertion.swift
+// Proton Pass - Created on 27/09/2024.
+// Copyright (c) 2024 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+
+import Client
+import Core
+
+protocol FetchItemsForTextInsertionUseCase: Sendable {
+    func execute(userId: String) async throws -> ItemsForTextInsertion
+}
+
+extension FetchItemsForTextInsertionUseCase {
+    func callAsFunction(userId: String) async throws -> ItemsForTextInsertion {
+        try await execute(userId: userId)
+    }
+}
+
+final class FetchItemsForTextInsertion: FetchItemsForTextInsertionUseCase {
+    private let symmetricKeyProvider: any SymmetricKeyProvider
+    private let accessRepository: any AccessRepositoryProtocol
+    private let itemRepository: any ItemRepositoryProtocol
+    private let shareRepository: any ShareRepositoryProtocol
+    private let logger: Logger
+
+    init(symmetricKeyProvider: any SymmetricKeyProvider,
+         accessRepository: any AccessRepositoryProtocol,
+         itemRepository: any ItemRepositoryProtocol,
+         shareRepository: any ShareRepositoryProtocol,
+         logManager: any LogManagerProtocol) {
+        self.symmetricKeyProvider = symmetricKeyProvider
+        self.accessRepository = accessRepository
+        self.itemRepository = itemRepository
+        self.shareRepository = shareRepository
+        logger = .init(manager: logManager)
+    }
+
+    func execute(userId: String) async throws -> ItemsForTextInsertion {
+        async let getSymmetricKey = symmetricKeyProvider.getSymmetricKey()
+        async let getPlan = accessRepository.getPlan(userId: userId)
+        async let getVaults = shareRepository.getVaults(userId: userId)
+        async let getEncryptedItems = itemRepository.getItems(userId: userId, state: .active)
+
+        let (symmetricKey, plan, vaults, encryptedItems) = try await (getSymmetricKey, getPlan, getVaults,
+                                                                      getEncryptedItems)
+
+        let applicableVaults = if plan.isFreeUser {
+            vaults.twoOldestVaults.allVaults
+        } else {
+            vaults
+        }
+
+        let applicableShareIds = applicableVaults.map(\.shareId)
+        let applicableEncryptedItems = encryptedItems.filter { item in
+            applicableShareIds.contains(where: { $0 == item.shareId })
+        }
+
+        logger.debug("Decrypting \(applicableEncryptedItems.count) items for user \(userId)")
+
+        let itemContents = try applicableEncryptedItems.compactMap {
+            try $0.getItemContent(symmetricKey: symmetricKey)
+        }
+
+        let searchableItems = itemContents.map { SearchableItem(from: $0, allVaults: applicableVaults) }
+        let items = itemContents.map(\.toItemUiModel)
+
+        return .init(userId: userId,
+                     vaults: applicableVaults,
+                     searchableItems: searchableItems,
+                     items: items)
+    }
+}
