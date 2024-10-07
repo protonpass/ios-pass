4824e63544fa906f328f0635737a5801018bc634
Martin Lukacs <martin.lukacs@proton.ch>
2024-10-07 10:34:10 +0200

    feat added advance alias amanagement feature flags

--- LocalPackages/Client/Sources/Client/Enumerations/FeatureFlagType.swift
+++ LocalPackages/Client/Sources/Client/Enumerations/FeatureFlagType.swift
@@ -31,4 +31,5 @@ public enum FeatureFlagType: String, FeatureFlagTypeProtocol {
     case passAccessKeyV1 = "PassAccessKeyV1"
     case passAccountSwitchV1 = "PassAccountSwitchV1"
     case passSimpleLoginAliasesSync = "PassSimpleLoginAliasesSync"
+    case passAdvancedAliasManagementV1 = "PassAdvancedAliasManagementV1"
 }
