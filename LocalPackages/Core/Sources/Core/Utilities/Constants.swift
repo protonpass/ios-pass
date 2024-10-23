a1e8215ea0fcf80615378b7b16f2aa7d6d800a94
Martin Lukacs <martin.lukacs@proton.ch>
2024-10-23 18:04:58 +0200

    fix: gitleak failing issue

--- LocalPackages/Core/Sources/Core/Utilities/Constants.swift
+++ LocalPackages/Core/Sources/Core/Utilities/Constants.swift
@@ -69,8 +69,8 @@ public enum Constants {
         public static let resetTipsStateOnLaunch = "resetTipsStateOnLaunch"
         public static let forceShowTips = "forceShowTips"
         public static let searchAndListSecureLink = "searchAndListSecureLink"
-        public static let forcePassPolicy = "forcePassPolicy"
-        public static let passPolicy = "passPolicy"
+        public static let forcePassPolicy = "forcePassPolicy" // #gitleaks:allow
+        public static let passPolicy = "passPolicy" // #gitleaks:allow
     }
 }
 
