3c75dfba51148e6ae73f029c60fcd4f54fdd3b59
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2024-11-04 14:00:10 +0100

    feat: toggle feature flags from QA menu

--- LocalPackages/Client/Sources/Client/Enumerations/FeatureFlagType.swift
+++ LocalPackages/Client/Sources/Client/Enumerations/FeatureFlagType.swift
@@ -25,7 +25,7 @@ import ProtonCoreFeatureFlags
 /// following is how a flag should be added:
 /// Example:
 ///    case passSharingV1 = "PassSharingV1"
-public enum FeatureFlagType: String, FeatureFlagTypeProtocol {
+public enum FeatureFlagType: String, CaseIterable, FeatureFlagTypeProtocol {
     case passSimpleLoginAliasesSync = "PassSimpleLoginAliasesSync"
     case passAdvancedAliasManagementV1 = "PassAdvancedAliasManagementV1"
 }
