e598e249f93bfe1cdeacf72790c174be3a4fc9e0
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2024-11-07 16:46:52 +0100

    feat(autofill): sort search results in background

--- AutoFill/Scenes/CredentialsView/CredentialSearchResultView.swift
+++ AutoFill/Scenes/CredentialsView/CredentialSearchResultView.swift
@@ -43,18 +43,30 @@ struct CredentialSearchResultView: View {
     }
 
     var body: some View {
-        headerView
+        Group {
+            switch viewModel.state {
+            case .loading:
+                ProgressView()
 
-        TableView(sections: viewModel.sections,
-                  configuration: .init(showSectionIndexTitles: selectedSortType.isAlphabetical,
-                                       rowSpacing: DesignConstant.sectionPadding / 2),
-                  id: viewModel.sections.hashValue,
-                  itemView: { item in
-                      GenericCredentialItemRow(item: item,
-                                               user: getUser(item),
-                                               selectItem: selectItem)
-                  },
-                  headerView: { _ in nil })
+            case let .loaded(results):
+                headerView
+                TableView(sections: results,
+                          configuration: .init(showSectionIndexTitles: selectedSortType.isAlphabetical,
+                                               rowSpacing: DesignConstant.sectionPadding / 2),
+                          id: results.hashValue,
+                          itemView: { item in
+                              GenericCredentialItemRow(item: item,
+                                                       user: getUser(item),
+                                                       selectItem: selectItem)
+                          },
+                          headerView: { _ in nil })
+
+            case let .error(error):
+                RetryableErrorView(errorMessage: error.localizedDescription,
+                                   onRetry: { viewModel.filterAndSortItems() })
+            }
+        }
+        .animation(.default, value: viewModel.state)
     }
 }
 
@@ -81,47 +93,85 @@ private typealias SearchResultSection = TableView<ItemSearchResult, GenericCrede
 
 @MainActor
 private final class CredentialSearchResultViewModel: ObservableObject {
-    @Published private(set) var sections: [SearchResultSection] = []
+    @Published private(set) var state: State = .loading
+
+    enum State: Equatable {
+        case loading
+        case loaded([SearchResultSection])
+        case error(any Error)
+
+        static func == (lhs: Self, rhs: Self) -> Bool {
+            switch (lhs, rhs) {
+            case (.loaded, .loaded), (.loading, .loading):
+                true
+            case let (.error(lError), .error(rError)):
+                lError.localizedDescription == rError.localizedDescription
+            default:
+                false
+            }
+        }
+    }
 
     let results: [ItemSearchResult]
+    private let sortType: SortType
+    private var task: Task<Void, Never>?
 
     init(results: [ItemSearchResult], sortType: SortType) {
         self.results = results
-        // TODO: handle error
-        try? filterAndSortItems(sortType)
+        self.sortType = sortType
+        filterAndSortItems()
     }
 }
 
-private extension CredentialSearchResultViewModel {
-    func filterAndSortItems(_ sortType: SortType) throws {
-        let type = Int.max
-        let sections: [SearchResultSection] = try {
-            switch sortType {
-            case .mostRecent:
-                let results = try results.mostRecentSortResult()
-                return results.buckets.map { bucket in
-                    .init(type: type,
-                          title: bucket.type.title,
-                          items: bucket.items)
+extension CredentialSearchResultViewModel {
+    func filterAndSortItems() {
+        task?.cancel()
+        task = Task.detached(priority: .userInitiated) { [weak self] in
+            guard let self else { return }
+            do {
+                await MainActor.run { [weak self] in
+                    guard let self else { return }
+                    state = .loading
                 }
+                let type = Int.max
+                let sections: [SearchResultSection] = try {
+                    switch sortType {
+                    case .mostRecent:
+                        let results = try results.mostRecentSortResult()
+                        return results.buckets.map { bucket in
+                            .init(type: type,
+                                  title: bucket.type.title,
+                                  items: bucket.items)
+                        }
 
-            case .alphabeticalAsc, .alphabeticalDesc:
-                let results = try results.alphabeticalSortResult(direction: sortType.sortDirection)
-                return results.buckets.map { bucket in
-                    .init(type: type,
-                          title: bucket.letter.character,
-                          items: bucket.items)
+                    case .alphabeticalAsc, .alphabeticalDesc:
+                        let results = try results.alphabeticalSortResult(direction: sortType.sortDirection)
+                        return results.buckets.map { bucket in
+                            .init(type: type,
+                                  title: bucket.letter.character,
+                                  items: bucket.items)
+                        }
+
+                    case .newestToOldest, .oldestToNewest:
+                        let results = try results.monthYearSortResult(direction: sortType.sortDirection)
+                        return results.buckets.map { bucket in
+                            .init(type: type,
+                                  title: bucket.monthYear.relativeString,
+                                  items: bucket.items)
+                        }
+                    }
+                }()
+                await MainActor.run { [weak self] in
+                    guard let self else { return }
+                    state = .loaded(sections.filter { !$0.items.isEmpty })
                 }
-
-            case .newestToOldest, .oldestToNewest:
-                let results = try results.monthYearSortResult(direction: sortType.sortDirection)
-                return results.buckets.map { bucket in
-                    .init(type: type,
-                          title: bucket.monthYear.relativeString,
-                          items: bucket.items)
+            } catch {
+                if error is CancellationError { return }
+                await MainActor.run { [weak self] in
+                    guard let self else { return }
+                    state = .error(error)
                 }
             }
-        }()
-        self.sections = sections.filter { !$0.items.isEmpty }
+        }
     }
 }
