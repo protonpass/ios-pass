7c2a09ea8b2a40dacf220d0a399b0e9cb0607263
Martin Lukacs <martin.lukacs@proton.ch>
2024-11-13 10:34:54 +0100

    feat: modification of telemetry and db implem for in app notif

--- LocalPackages/Client/Sources/Client/CoreData/Entities/TelemetryEventEntity.swift
+++ LocalPackages/Client/Sources/Client/CoreData/Entities/TelemetryEventEntity.swift
@@ -33,23 +33,30 @@ extension TelemetryEventEntity {
     }
 
     @NSManaged var uuid: String
-    @NSManaged var rawValue: String
+    @NSManaged var rawValue: String?
     @NSManaged var time: Double
     @NSManaged var userID: String
+    @NSManaged var rawData: Data?
 }
 
 extension TelemetryEventEntity {
     func toTelemetryEvent() throws -> TelemetryEvent {
-        guard let type = TelemetryEventType(rawValue: rawValue) else {
+        var type: TelemetryEventType
+        if let rawData {
+            type = try JSONDecoder().decode(TelemetryEventType.self, from: rawData)
+        } else if let rawValue, let decodedType = TelemetryEventType(rawValue: rawValue) {
+            type = decodedType
+        } else {
             throw PassError.coreData(.corrupted(object: self, property: "rawValue"))
         }
+
         return .init(uuid: uuid, time: time, type: type)
     }
 
-    func hydrate(from event: TelemetryEvent, userId: String) {
+    func hydrate(from event: TelemetryEvent, userId: String) throws {
         uuid = event.uuid
-        rawValue = event.type.rawValue
         time = event.time
         userID = userId
+        rawData = try JSONEncoder().encode(event.type)
     }
 }
---
+++ LocalPackages/Entities/Tests/EntitiesTests/Models/TelemetryEventTests.swift
@@ -0,0 +1,74 @@
+//
+// TelemetryEventTests.swift
+// Proton Pass - Created on 12/11/2024.
+// Copyright (c) 2024 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+
+@testable import Entities
+import Testing
+import Foundation
+
+@Suite(.tags(.entity))
+struct TelemetryEventTypeTests {
+    let encoder = JSONEncoder()
+    let decoder = JSONDecoder()
+
+    @Test("TelemetryEventType can be encoded and decoded",
+          arguments: [
+            TelemetryEventType.create(.login),  // Replace `.someContentType` with actual value for ItemContentType
+                          .read(.login),
+                          .update(.login),
+                          .delete(.login),
+                          .autofillDisplay,
+                          .autofillTriggeredFromSource,
+                          .autofillTriggeredFromApp,
+                          .searchTriggered,
+                          .searchClick,
+                          .twoFaCreation,
+                          .twoFaUpdate,
+                          .passkeyCreate,
+                          .passkeyAuth,
+                          .passkeyDisplay,
+                          .monitorDisplayHome,
+                          .monitorDisplayWeakPasswords,
+                          .monitorDisplayReusedPasswords,
+                          .monitorDisplayMissing2FA,
+                          .monitorDisplayExcludedItems,
+                          .monitorDisplayDarkWebMonitoring,
+                          .monitorDisplayMonitoringProtonAddresses,
+                          .monitorDisplayMonitoringEmailAliases,
+                          .monitorAddCustomEmailFromSuggestion,
+                          .monitorItemDetailFromWeakPassword,
+                          .monitorItemDetailFromMissing2FA,
+                          .monitorItemDetailFromReusedPassword,
+                          .multiAccountAddAccount,
+                          .multiAccountRemoveAccount,
+                          .passNotificationDisplayNotification(notificationKey: "exampleKey"),
+                          .passNotificationChangeNotificationStatus(notificationKey: "exampleKey", notificationStatus: 1),
+                          .passNotificationNotificationCtaClick(notificationKey: "exampleKey")
+                      ])
+    func encodingDecodingTelemetryEventType(type: TelemetryEventType) throws {
+                    // Test encoding
+                    let encodedData = try encoder.encode(type)
+                    let encodedString = String(data: encodedData, encoding: .utf8)!
+                    print("Encoded JSON for \(type): \(encodedString)")
+        
+                    // Test decoding
+                    let decodedEvent = try decoder.decode(TelemetryEventType.self, from: encodedData)
+                    #expect(type == decodedEvent)
+    }
+}
