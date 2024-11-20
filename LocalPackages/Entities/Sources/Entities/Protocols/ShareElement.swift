abcb7f551aa5c5bb07c0cad9a5d2e87fc6542685
Martin Lukacs <martin.lukacs@proton.ch>
2024-11-20 17:40:01 +0100

    feat: beginning of item sharing

---
+++ LocalPackages/Entities/Sources/Entities/Protocols/ShareElement.swift
@@ -0,0 +1,38 @@
+//
+// ShareElement.swift
+// Proton Pass - Created on 19/11/2024.
+// Copyright (c) 2024 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+
+import Foundation
+
+// Protocol to define shared properties and behaviors
+public protocol ShareElementProtocol: Identifiable, Hashable, Equatable, Sendable {
+    var id: String { get }
+    var shareId: String { get }
+    var addressId: String { get }
+    var name: String { get }
+    var isOwner: Bool { get }
+    var shareRole: ShareRole { get }
+    var members: Int { get }
+    var maxMembers: Int { get }
+    var pendingInvites: Int { get }
+    var newUserInvitesReady: Int { get }
+    var shared: Bool { get }
+    var createTime: Int64 { get }
+    var canAutoFill: Bool { get }
+}
