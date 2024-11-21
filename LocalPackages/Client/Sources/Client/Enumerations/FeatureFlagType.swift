26aeb4e47fe8404dae0e9dffcbf595aa55e230b5
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2024-11-21 15:10:59 +0100

    feat: put better authentication behind a feature flag

--- LocalPackages/Client/Sources/Client/Enumerations/FeatureFlagType.swift
+++ LocalPackages/Client/Sources/Client/Enumerations/FeatureFlagType.swift
@@ -29,4 +29,5 @@ public enum FeatureFlagType: String, CaseIterable, FeatureFlagTypeProtocol {
     case passSimpleLoginAliasesSync = "PassSimpleLoginAliasesSync"
     case passAdvancedAliasManagementV1 = "PassAdvancedAliasManagementV1"
     case passInAppMessagesV1 = "PassInAppMessagesV1"
+    case passIOSBetterAuthentication = "PassIOSBetterAuthentication"
 }
