1fcf3bacd8fd39ab891c1ec8be7b2caf83a8bdc6
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2024-12-10 11:38:37 +0100

    feat: encrypt file use case

---
+++ LocalPackages/UseCases/Sources/UseCases/FileAttachments/EncryptFile.swift
@@ -0,0 +1,46 @@
+//
+// EncryptFile.swift
+// Proton Pass - Created on 09/12/2024.
+// Copyright (c) 2024 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+//
+
+import Client
+import CryptoKit
+import Foundation
+
+/// Symetrically encrypt a file at a given URL and save to a destination URL
+public protocol EncryptFileUseCase: Sendable {
+    func execute(key: Data, sourceUrl: URL, destinationUrl: URL) async throws
+}
+
+public extension EncryptFileUseCase {
+    func callAsFunction(key: Data, sourceUrl: URL, destinationUrl: URL) async throws {
+        try await execute(key: key, sourceUrl: sourceUrl, destinationUrl: destinationUrl)
+    }
+}
+
+public final class EncryptFile: EncryptFileUseCase {
+    public init() {}
+
+    public func execute(key: Data, sourceUrl: URL, destinationUrl: URL) async throws {
+        let data = try Data(contentsOf: sourceUrl)
+        let encryptedData = try AES.GCM.seal(data, key: key, associatedData: .fileData)
+        FileManager.default.createFile(atPath: destinationUrl.path(),
+                                       contents: encryptedData.combined)
+    }
+}
