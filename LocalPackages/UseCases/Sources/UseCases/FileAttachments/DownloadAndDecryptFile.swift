f6f563593ba162b55ca649ec15d870f605c1669c
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2024-12-19 14:22:50 +0100

    feat: download file chunk by chunk

--- LocalPackages/UseCases/Sources/UseCases/FileAttachments/DownloadAndDecryptFile.swift
+++ LocalPackages/UseCases/Sources/UseCases/FileAttachments/DownloadAndDecryptFile.swift
@@ -42,28 +42,23 @@ public extension DownloadAndDecryptFileUseCase {
 public final class DownloadAndDecryptFile: DownloadAndDecryptFileUseCase {
     private let keyManager: any PassKeyManagerProtocol
     private let remoteDatasource: any RemoteFileDatasourceProtocol
-    private let decryptFile: any DecryptFileUseCase
 
     public init(keyManager: any PassKeyManagerProtocol,
-                remoteDatasource: any RemoteFileDatasourceProtocol,
-                decryptFile: any DecryptFileUseCase) {
+                remoteDatasource: any RemoteFileDatasourceProtocol) {
         self.keyManager = keyManager
         self.remoteDatasource = remoteDatasource
-        self.decryptFile = decryptFile
     }
 
     public func execute(userId: String,
                         item: any ItemIdentifiable,
                         file: ItemFile) async throws -> URL {
-        guard let chunkId = file.chunks.first?.chunkID else {
-            throw PassError.fileAttachment(.noChunkId(file.fileID))
-        }
-
         guard let name = file.name else {
             throw PassError.fileAttachment(.failedToDownloadMissingFileName(file.fileID))
         }
 
-        let url = FileManager.default.temporaryDirectory
+        let fileManager = FileManager.default
+
+        let url = fileManager.temporaryDirectory
             .appending(path: userId)
             .appending(path: item.shareId)
             .appending(path: item.itemId)
@@ -71,7 +66,8 @@ public final class DownloadAndDecryptFile: DownloadAndDecryptFileUseCase {
             .appending(path: "\(file.modifyTime)")
             .appendingPathComponent(name, conformingTo: .data)
 
-        if FileManager.default.fileExists(atPath: url.path()) {
+        if fileManager.fileExists(atPath: url.path()) {
+            // File already downloaded and cached
             return url
         }
 
@@ -91,12 +87,24 @@ public final class DownloadAndDecryptFile: DownloadAndDecryptFileUseCase {
                                        key: itemKey.keyData,
                                        associatedData: .fileKey)
 
-        let encryptedFileData = try await remoteDatasource.getChunkContent(userId: userId,
-                                                                           item: item,
-                                                                           fileId: file.fileID,
-                                                                           chunkId: chunkId)
+        // Create the empty file
+        try fileManager.createDirectory(at: url.deletingLastPathComponent(),
+                                        withIntermediateDirectories: true)
+        fileManager.createFile(atPath: url.path(), contents: nil)
 
-        try await decryptFile(key: fileKey, data: encryptedFileData, destinationUrl: url)
+        // Download, decrypt and write to file chunk by chunk
+        let fileHandle = try FileHandle(forWritingTo: url)
+        defer { try? fileHandle.close() }
+        for chunk in file.chunks {
+            let encrypted = try await remoteDatasource.getChunkContent(userId: userId,
+                                                                       item: item,
+                                                                       fileId: file.fileID,
+                                                                       chunkId: chunk.chunkID)
+            let decrypted = try AES.GCM.open(encrypted,
+                                             key: fileKey,
+                                             associatedData: .fileData)
+            try fileHandle.write(contentsOf: decrypted)
+        }
         return url
     }
 }
