bc66efff8d2fe2675a934c4c24c0b086107e1779
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2024-12-30 10:24:45 +0100

    feat: get item files for all revisions function

--- iOS/Scenes/Homepage/Create & Edit Items/BaseCreateEditItemViewModel.swift
+++ iOS/Scenes/Homepage/Create & Edit Items/BaseCreateEditItemViewModel.swift
@@ -620,8 +620,6 @@ extension BaseCreateEditItemViewModel: FileAttachmentsEditHandler {
             case let .item(itemFile):
                 filePreviewMode = .item(itemFile, self, .none)
             }
-        } catch {
-            handle(error)
         }
     }
 
