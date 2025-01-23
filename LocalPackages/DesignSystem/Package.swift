8f2c1dbe19bd425c31c41f7bf163266d2282ea74
Alex Morral <alex.morral@proton.ch>
2025-01-23 18:00:57 +0100

    feat: Bump ProtonCore to 29.0.2

--- LocalPackages/DesignSystem/Package.swift
+++ LocalPackages/DesignSystem/Package.swift
@@ -21,7 +21,7 @@ let package = Package(name: "DesignSystem",
                       ],
                       dependencies: [
                           // Dependencies declare other packages that this package depends on.
-                        .package(url: "https://github.com/ProtonMail/protoncore_ios", exact: "29.0.1"),
+                        .package(url: "https://github.com/ProtonMail/protoncore_ios", exact: "29.0.2"),
                         .package(url: "https://github.com/jdg/MBProgressHUD", exact: "1.2.0"),
                         .package(name: "Macro", path: "../Macro")
                       ],
