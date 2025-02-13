ea435a582b94dc5b4cdb5ed390b41dca20d08386
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2025-02-13 13:23:09 +0100

    feat: parse note column to support new Chrome CSV format

--- LocalPackages/UseCases/Tests/UseCasesTests/Items/ParseCsvLoginsTests.swift
+++ LocalPackages/UseCases/Tests/UseCasesTests/Items/ParseCsvLoginsTests.swift
@@ -33,14 +33,14 @@ struct ParseCsvLoginsTests {
     func invalidNumberOfColumn() async throws {
         // Given
         let csv = """
-        name,url,username,password,other
+        name,url,username,password,other,column
         proton.me,https://account.proton.me/switch,nobody@proton.me,proton123
         missing url,,missingurl@proton.me,proton123
         missing password,https://account.proton.me/switch,missingpw,
         broken url,htt:/proton.me/switch,brokenurl@proton.me,
         """
 
-        await #expect(throws: PassError.csv(.invalidNumberOfColumn(5))) {
+        await #expect(throws: PassError.csv(.invalidNumberOfColumn(6))) {
             try await sut(csv)
         }
     }
@@ -79,8 +79,8 @@ struct ParseCsvLoginsTests {
         }
     }
 
-    @Test("Success")
-    func success() async throws {
+    @Test("Success without note column")
+    func successWithoutNoteColumn() async throws {
         // Given
         let csv = """
         name,url,username,password
@@ -101,23 +101,73 @@ struct ParseCsvLoginsTests {
         #expect(logins[0].email == "nobody@proton.me")
         #expect(logins[0].username.isEmpty)
         #expect(logins[0].password == "proton123")
+        #expect(logins[0].note.isEmpty)
 
         #expect(logins[1].name == "missing url")
         #expect(logins[1].url.isEmpty)
         #expect(logins[1].email == "missingurl@proton.me")
         #expect(logins[1].username.isEmpty)
         #expect(logins[1].password == "proton, 123")
+        #expect(logins[1].note.isEmpty)
 
         #expect(logins[2].name == "missing password")
         #expect(logins[2].url == "https://account.proton.me/switch")
         #expect(logins[2].email.isEmpty)
         #expect(logins[2].username == "missingpw")
         #expect(logins[2].password.isEmpty)
+        #expect(logins[2].note.isEmpty)
 
         #expect(logins[3].name == "broken url")
         #expect(logins[3].url == "htt:/proton.me/switch")
         #expect(logins[3].email == "brokenurl@proton.me")
         #expect(logins[3].username.isEmpty)
         #expect(logins[3].password.isEmpty)
+        #expect(logins[3].note.isEmpty)
+    }
+
+    @Test("Success with note column")
+    func successWithNoteColumn() async throws {
+        // Given
+        let csv = """
+        name,url,username,password,note
+        proton.me,https://account.proton.me/switch,nobody@proton.me,proton123,"Note 1"
+        missing url,,missingurl@proton.me,"proton, 123",Note 2
+        missing password,https://account.proton.me/switch,missingpw,,
+        broken url,htt:/proton.me/switch,brokenurl@proton.me,,Note 4
+        """
+
+        // When
+        let logins = try await sut(csv)
+
+        // Then
+        #expect(logins.count == 4)
+
+        #expect(logins[0].name == "proton.me")
+        #expect(logins[0].url == "https://account.proton.me/switch")
+        #expect(logins[0].email == "nobody@proton.me")
+        #expect(logins[0].username.isEmpty)
+        #expect(logins[0].password == "proton123")
+        #expect(logins[0].note == "Note 1")
+
+        #expect(logins[1].name == "missing url")
+        #expect(logins[1].url.isEmpty)
+        #expect(logins[1].email == "missingurl@proton.me")
+        #expect(logins[1].username.isEmpty)
+        #expect(logins[1].password == "proton, 123")
+        #expect(logins[1].note == "Note 2")
+
+        #expect(logins[2].name == "missing password")
+        #expect(logins[2].url == "https://account.proton.me/switch")
+        #expect(logins[2].email.isEmpty)
+        #expect(logins[2].username == "missingpw")
+        #expect(logins[2].password.isEmpty)
+        #expect(logins[2].note.isEmpty)
+
+        #expect(logins[3].name == "broken url")
+        #expect(logins[3].url == "htt:/proton.me/switch")
+        #expect(logins[3].email == "brokenurl@proton.me")
+        #expect(logins[3].username.isEmpty)
+        #expect(logins[3].password.isEmpty)
+        #expect(logins[3].note == "Note 4")
     }
 }
