e0764904f008873464561b1f9c57e020fd5219af
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2025-02-27 21:25:25 +0100

    chore: mutualize some translations

--- iOS/Scenes/Homepage/Create & Edit Items/Custom item/CustomItemTemplatesList.swift
+++ iOS/Scenes/Homepage/Create & Edit Items/Custom item/CustomItemTemplatesList.swift
@@ -154,10 +154,11 @@ private extension CustomItemTemplate {
         case .softwareLicense:
             .init(icon: IconProvider.fileLines, title: "Software License")
         case .sshKey:
-            .init(icon: IconProvider.filingCabinet, title: "SSH Key")
+            .init(icon: IconProvider.filingCabinet,
+                  title: LocalizedStringKey("SSH key".capitalized))
         case .wifi:
             .init(icon: UIImage(systemName: "wifi") ?? IconProvider.shield,
-                  title: "WiFi Network")
+                  title: LocalizedStringKey("WiFi network".capitalized))
         case .bankAccount:
             .init(icon: PassIcon.bank, title: "Bank Account")
         case .cryptoWallet:
@@ -173,7 +174,8 @@ private extension CustomItemTemplate {
         case .rewardProgram:
             .init(icon: IconProvider.bagPercent, title: "Reward Program")
         case .socialSecurityNumber:
-            .init(icon: IconProvider.users, title: "Social Security Number")
+            .init(icon: IconProvider.users,
+                  title: LocalizedStringKey("Social security sumber".capitalized))
         }
     }
 }
