dd5f5b9ab5ae952c3f84eeef56eba1691b519166
LisaBot <service_account_4adb237cb9e6ed41ddc75bf6b371d140@noreply.gitlab.protontech.ch>
2025-04-04 11:26:20 +0000

    chore(deps): update dependency protonmail/protoncore_ios to v30.0.4

--- LocalPackages/DesignSystem/Package.swift
+++ LocalPackages/DesignSystem/Package.swift
@@ -21,7 +21,7 @@ let package = Package(name: "DesignSystem",
                       ],
                       dependencies: [
                           // Dependencies declare other packages that this package depends on.
-                        .package(url: "https://github.com/ProtonMail/protoncore_ios", exact: "30.0.2"),
+                        .package(url: "https://github.com/ProtonMail/protoncore_ios", exact: "30.0.4"),
                         .package(url: "https://github.com/jdg/MBProgressHUD", exact: "1.2.0"),
                         .package(name: "Macro", path: "../Macro")
                       ],
