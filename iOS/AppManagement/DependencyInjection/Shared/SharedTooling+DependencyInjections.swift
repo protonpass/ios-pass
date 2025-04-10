604156c74c52e5837b7b336722d70cc913a3b021
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2025-04-10 16:07:00 +0200

    chore: remove RandomUuidProviderProtocol

--- iOS/AppManagement/DependencyInjection/Shared/SharedTooling+DependencyInjections.swift
+++ iOS/AppManagement/DependencyInjection/Shared/SharedTooling+DependencyInjections.swift
@@ -131,10 +131,6 @@ extension SharedToolingContainer {
         self { CurrentDateProvider() }
     }
 
-    var randomUuidProvider: Factory<any RandomUuidProviderProtocol> {
-        self { RandomUuidProvider() }
-    }
-
     var preferencesManager: Factory<any PreferencesManagerProtocol> {
         self {
             let cont = SharedRepositoryContainer.shared
