357893533327baf967438206b51e6d795cd1f78b
Tim Palade <timpalade@Tims-MacBook-Pro.local>
2025-05-01 16:24:51 +0300

    Upgrade to Core version 32.0.4

--- LocalPackages/UseCases/Package.swift
+++ LocalPackages/UseCases/Package.swift
@@ -30,7 +30,7 @@ let package = Package(name: "UseCases",
                           .package(name: "Client", path: "../Client"),
                           .package(name: "PassRustCore", path: "../PassRustCore"),
                           .package(url: "https://github.com/getsentry/sentry-cocoa.git", exact: "8.40.1"),
-                          .package(url: "https://github.com/ProtonMail/protoncore_ios", exact: "32.0.2")
+                          .package(url: "https://github.com/ProtonMail/protoncore_ios", exact: "32.0.4")
                       ],
                       targets: [
                           // Targets are the basic building blocks of a package. A target can define a module or a
