828bee25c51cebd5c378ffa001374c2565a813ab
Tim Palade <timpalade@Tims-MacBook-Pro.local>
2025-05-02 16:28:37 +0300

    Update core to 32.0.5

--- LocalPackages/Core/Package.swift
+++ LocalPackages/Core/Package.swift
@@ -24,7 +24,7 @@ let package = Package(name: "Core",
                                    targets: ["CoreMocks"])
                       ],
                       dependencies: [
-                          .package(url: "https://github.com/ProtonMail/protoncore_ios", exact: "32.0.4"),
+                          .package(url: "https://github.com/ProtonMail/protoncore_ios", exact: "32.0.5"),
                           .package(name: "Entities", path: "../Entities"),
                           .package(name: "DesignSystem", path: "../DesignSystem"),
                           .package(name: "Macro", path: "../Macro")
