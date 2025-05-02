b812b4ccf0d17e95c147c6a0f7c26391f0e9ebbb
Tim Palade <timpalade@Tims-MacBook-Pro.local>
2025-05-02 13:59:39 +0300

    Check feature flag and edmOptOut before showing the Sign in to another device setting

--- iOS/Scenes/Homepage/Profile Tab/ProfileTabView.swift
+++ iOS/Scenes/Homepage/Profile Tab/ProfileTabView.swift
@@ -175,7 +175,9 @@ struct ProfileTabView: View {
                 .roundedEditableSection()
                 .accessibilityIdentifierLeaf("AccountCell")
 
-                signInToAnotherDeviceSection
+                if viewModel.isEasyDeviceMigrationEnabled {
+                    signInToAnotherDeviceSection
+                }
             }.padding()
         }
     }
