dd6a968d9a46f975585ffd23d86c7fc2ab510681
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2025-05-13 14:33:20 +0200

    bump: core 32.1.0

--- LocalPackages/TestingToolkit/Package.swift
+++ LocalPackages/TestingToolkit/Package.swift
@@ -42,7 +42,7 @@ let package = Package(name: "TestingToolkit",
                       dependencies: [
                           // Dependencies declare other packages that this package depends on.
                           .package(name: "Entities", path: "../Entities"),
-                          .package(url: "https://github.com/ProtonMail/protoncore_ios", exact: "32.0.5")
+                          .package(url: "https://github.com/ProtonMail/protoncore_ios", exact: "32.1.0")
                       ],
                       targets: [
                           // Targets are the basic building blocks of a package. A target can define a module or a
