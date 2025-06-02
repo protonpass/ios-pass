eba5ee9ec02fe5313f5d4cee5b467ac36e908c83
Richard Gasztany <richard.gasztany@proton.ch>
2025-06-02 14:07:25 +0200

    TPE-1061 - update mocks support chronological order

--- iOSUITests/MockTests/MockBaseTestCase.swift
+++ iOSUITests/MockTests/MockBaseTestCase.swift
@@ -78,4 +78,9 @@ class MockBaseTestCase: ProtonCoreBaseTestCase {
 
         LogoutRobot().logoutIfNeeded()
     }
+
+    func generateRandomString(length: Int) -> String {
+        let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        return String((0..<length).compactMap { _ in characters.randomElement() })
+    }
 }
