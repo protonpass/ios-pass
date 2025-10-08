7514565a9f91d3259a5c49c3d30b02e6ed0ad734
LisaBot <service_account_4adb237cb9e6ed41ddc75bf6b371d140@noreply.gitlab.protontech.ch>
2025-10-08 17:52:29 +0000

    chore(deps): update dependency apple/swift-protobuf to from: "1.32.0"

--- LocalPackages/Entities/Package.swift
+++ LocalPackages/Entities/Package.swift
@@ -27,7 +27,7 @@ let package = Package(name: "Entities",
                       dependencies: [
                           // Dependencies declare other packages that this package depends on.
                           // .package(url: /* package url */, from: "1.0.0"),
-                        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.31.1")
+                        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.32.0")
                       ],
                       targets: [
                           // Targets are the basic building blocks of a package. A target can define a module or a
