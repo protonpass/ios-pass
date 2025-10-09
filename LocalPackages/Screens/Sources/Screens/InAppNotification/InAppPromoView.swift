f59def42a3b576f433dd0f7eeea81e3ecbf3a5a6
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2025-10-09 16:45:42 +0200

    feat: render promo contents

---
+++ LocalPackages/Screens/Sources/Screens/InAppNotification/InAppPromoView.swift
@@ -0,0 +1,126 @@
+//
+// InAppPromoView.swift
+// Proton Pass - Created on 08/10/2025.
+// Copyright (c) 2025 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+
+import DesignSystem
+import Entities
+import ProtonCoreUIFoundations
+import SwiftUI
+
+public struct InAppPromoView: View {
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.colorScheme) private var colorScheme
+    private let notification: InAppNotification
+    private let promoContents: InAppNotificationPromoContents
+    private let onAppear: () -> Void
+    private let onMinimize: () -> Void
+    private let onDismiss: () -> Void
+
+    private var themedContents: InAppNotificationPromoThemedContents {
+        colorScheme == .dark ?
+            promoContents.darkThemeContents : promoContents.lightThemeContents
+    }
+
+    public init(notification: InAppNotification,
+                promoContents: InAppNotificationPromoContents,
+                onAppear: @escaping () -> Void,
+                onMinimize: @escaping () -> Void,
+                onDismiss: @escaping () -> Void) {
+        self.notification = notification
+        self.promoContents = promoContents
+        self.onAppear = onAppear
+        self.onMinimize = onMinimize
+        self.onDismiss = onDismiss
+    }
+
+    public var body: some View {
+        ZStack {
+            backgroundImage
+                .ignoresSafeArea(.all)
+
+            VStack {
+                Spacer()
+                contentImage
+                Spacer()
+                Button(action: {
+                    dismiss()
+                    onMinimize()
+                }, label: {
+                    Text(verbatim: promoContents.closePromoText)
+                        .foregroundStyle(foregroundColor)
+                })
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding()
+
+            dismissButton
+        }
+        .onAppear(perform: onAppear)
+        .interactiveDismissDisabled()
+    }
+}
+
+private extension InAppPromoView {
+    var backgroundImage: some View {
+        AsyncImage(url: URL(string: themedContents.backgroundImageUrl),
+                   content: { image in
+                       image
+                           .resizable()
+                   }, placeholder: {
+                       EmptyView()
+                   })
+    }
+
+    var contentImage: some View {
+        AsyncImage(url: URL(string: themedContents.contentImageUrl),
+                   content: { image in
+                       image
+                           .resizable()
+                           .scaledToFit()
+                   }, placeholder: {
+                       ProgressView()
+                   })
+    }
+
+    var dismissButton: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Button(action: {
+                    dismiss()
+                    onDismiss()
+                }, label: {
+                    Image(uiImage: IconProvider.crossCircleFilled)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 32)
+                        .foregroundStyle(foregroundColor)
+                        .padding(.trailing, 16)
+                        .padding(.top, UIDevice.current.isIpad ? 16 : 0)
+                })
+            }
+            Spacer()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+
+    var foregroundColor: Color {
+        Color(hex: themedContents.closePromoTextColor)
+    }
+}
