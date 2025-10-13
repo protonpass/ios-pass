e8bd35cacab392e0167810f130c94482f78e9b83
Martin Lukacs <martin.lukacs@proton.ch>
2025-10-13 10:47:55 +0200

    feat: beginning of shared vault alert item creation

---
+++ iOS/Scenes/Homepage/Profile Tab/QA Features/ResetAlertDisplaySection.swift
@@ -0,0 +1,95 @@
+//
+// ResetAlertDisplaySection.swift
+// Proton Pass - Created on 13/10/2025.
+// Copyright (c) 2025 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+
+// var dismissedUIElements = preferencesManager.appPreferences.unwrapped().dismissedElements
+// dismissedUIElements.dismissedElements[.itemCreationInSharedVaultAlert] = true
+// try await preferencesManager.updateAppPreferences(\.dismissedElements,
+//                                                  value: dismissedUIElements)
+
+import Combine
+import Core
+import DesignSystem
+import Entities
+import FactoryKit
+import SwiftUI
+
+// swiftlint:disable identifier_name line_length
+struct ResetAlertDisplaySection: View {
+    var body: some View {
+        NavigationLink(destination: { ResetAlertDisplayView() },
+                       label: { Text(verbatim: "Password Policy") })
+    }
+}
+
+private struct ResetAlertDisplayView: View {
+    @StateObject private var viewModel = ResetAlertDisplayViewModel()
+
+    @AppStorage(Constants.QA.forcePasswordPolicy)
+    private var forcePasswordPolicy = false
+
+    var body: some View {
+        List {
+            Section(header: Text(verbatim: "Alert dismissed state settings").font(.headline.bold())) {
+                ForEach(DismissibleUIElementId.allCases) { dismissibleUIElementId in
+                    switch dismissibleUIElementId {
+                    case .itemCreationInSharedVaultAlert:
+                        Toggle(isOn: $sharedVaultItemCreationDismissed,
+                               label: { Text(verbatim: "Item creation in shared vault alert dismissed") })
+                    }
+                }
+            }
+        }
+        .padding(DesignConstant.sectionPadding)
+        .navigationTitle(Text(verbatim: "Password Policy Settings"))
+    }
+}
+
+@MainActor
+private final class ResetAlertDisplayViewModel: ObservableObject {
+    @Published var sharedVaultItemCreationDismissed = false {
+        didSet {
+            updateAlert()
+        }
+    }
+
+    @LazyInjected(\SharedToolingContainer.preferencesManager) var preferencesManager
+
+    init() {
+        updateValues()
+    }
+
+    private func updateValues() {
+        var dismissedUIElements = preferencesManager.appPreferences.unwrapped().dismissedElements
+
+        sharedVaultItemCreationDismissed = dismissedUIElements
+            .dismissedElements[.itemCreationInSharedVaultAlert] ?? false
+    }
+
+    func updateAlert() {
+        var dismissedUIElements = preferencesManager.appPreferences.unwrapped().dismissedElements
+        dismissedUIElements.dismissedElements[.itemCreationInSharedVaultAlert] = sharedVaultItemCreationDismissed
+        Task {
+            try? await preferencesManager.updateAppPreferences(\.dismissedElements,
+                                                               value: dismissedUIElements)
+        }
+    }
+}
+
+// swiftlint:enable identifier_name line_length
