cb6f1cf93dc1f0bd575b4ba04df0e0ffc131c609
Martin Lukacs <martin.lukacs@proton.ch>
2026-06-04 10:12:48 +0200

    feat: bump proton core lib to 37.1.1

--- LocalPackages/UseCases/Package.swift
+++ LocalPackages/UseCases/Package.swift
@@ -32,7 +32,7 @@ let package = Package(name: "UseCases",
                           .package(name: "Client", path: "../Client"),
                           .package(name: "PassRustCore", path: "../PassRustCore"),
                           .package(url: "https://github.com/getsentry/sentry-cocoa.git", exact: "9.15.0"),
-                          .package(url: "https://github.com/ProtonMail/protoncore_ios", from: "36.0.3")
+                          .package(url: "https://github.com/ProtonMail/protoncore_ios", from: "37.1.1")
                       ],
                       targets: [
                           // Targets are the basic building blocks of a package. A target can define a module or a
