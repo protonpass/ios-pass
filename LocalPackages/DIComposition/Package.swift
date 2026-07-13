9f594950d753b55f7eafde8c8938c9353d139764
Martin Lukacs <martin.lukacs@proton.ch>
2026-07-13 11:54:20 +0200

    feat: bump factory version to 3.3.1

--- LocalPackages/DIComposition/Package.swift
+++ LocalPackages/DIComposition/Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         .package(name: "UseCases", path: "../UseCases"),
         .package(name: "Client", path: "../Client"),
         .package(url: "https://github.com/ProtonMail/protoncore_ios", from: "37.4.0"),
-        .package(url: "https://github.com/hmlongco/Factory", exact: "3.2.3"),
+        .package(url: "https://github.com/hmlongco/Factory", exact: "3.3.1"),
         .package(name: "PassRustCore", path: "../PassRustCore"),
     ],
     targets: [
