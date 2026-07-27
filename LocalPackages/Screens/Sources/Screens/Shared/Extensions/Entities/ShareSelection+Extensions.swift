86438203b5f4eff87398c26a1a821c405431a04e
Martin Lukacs <martin.lukacs@proton.ch>
2026-07-27 17:58:07 +0200

    feat: moving navigation menu in screens

---
+++ LocalPackages/Screens/Sources/Screens/Shared/Extensions/Entities/ShareSelection+Extensions.swift
@@ -0,0 +1,111 @@
+//
+// ShareSelection+Extensions.swift
+// Proton Pass - Created on 27/07/2026.
+// Copyright (c) 2026 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+
+import DesignSystem
+import Entities
+import Macro
+import ProtonCoreUIFoundations
+import SwiftUI
+
+public extension ShareSelection {
+    var title: String {
+        switch self {
+        case .all:
+            #localized("All items", bundle: .module)
+
+        case let .precise(selection):
+            selection.title
+
+        case .trash:
+            #localized("Trash", bundle: .module)
+
+        case .sharedByMe:
+            #localized("Shared by me", bundle: .module)
+
+        case .sharedWithMe:
+            #localized("Shared with me", bundle: .module)
+        }
+    }
+
+    var icon: Image {
+        switch self {
+        case .all:
+            PassIcon.brandPass
+
+        case let .precise(selection):
+            selection.share.vaultBigIcon ?? PassIcon.vaultIcon1Big
+
+        case .trash:
+            IconProvider.trash
+
+        case .sharedByMe:
+            IconProvider.userArrowRight
+
+        case .sharedWithMe:
+            IconProvider.userArrowLeft
+        }
+    }
+
+    var color: Color {
+        switch self {
+        case .all, .sharedByMe, .sharedWithMe:
+            PassColor.interactionNormMajor2
+
+        case let .precise(selection):
+            selection.share.mainColor ?? PassColor.textWeak
+
+        case .trash:
+            PassColor.textWeak
+        }
+    }
+
+    var share: Share? {
+        switch self {
+        case let .precise(selection):
+            selection.share
+
+        default:
+            nil
+        }
+    }
+
+    var accessibilityLabel: String {
+        switch self {
+        case .all:
+            #localized("Show all vaults", bundle: .module)
+
+        case let .precise(selection):
+            if selection.isFolderSelected {
+                #localized("Show %@ folder", bundle: .module, selection.title)
+            } else {
+                #localized("Show %@ vault", bundle: .module, selection.title)
+            }
+
+        case .trash:
+            #localized("Show trash", bundle: .module)
+
+        case .sharedByMe:
+            #localized("Show shared by me", bundle: .module)
+
+        case .sharedWithMe:
+            #localized("Show shared with me", bundle: .module)
+        }
+    }
+}
