6b485c1b13dd929323a778404a5a57fce1a47515
Martin Lukacs <martin.lukacs@proton.ch>
2026-09-21 14:57:56 +0200

    fix: periphery

--- LocalPackages/UseCases/Sources/UseCases/Folder/UserHasRemoteFolders.swift
+++ LocalPackages/UseCases/Sources/UseCases/Folder/UserHasRemoteFolders.swift
@@ -73,7 +73,7 @@ public struct UserHasRemoteFolders: UserHasRemoteFoldersUseCase {
     }
 }
 
-public enum UserHasRemoteFoldersError: Error {
+enum UserHasRemoteFoldersError: Error {
     /// At least one share could not be reached, so the absence of folders is unproven.
     case incompleteScan
 }
